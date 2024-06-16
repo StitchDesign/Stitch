da8bb0e3fa6c0d8661733d7034e334c313df0a02
Chris Clampitt <christian.clampitt@nyu.edu>
2024-06-16 13:16:14 -0600

    Reading frame of inspector vs frame of view inside inspector

--- Stitch/Graph/LayerInspector/LayerInspectorView.swift
+++ Stitch/Graph/LayerInspector/LayerInspectorView.swift
@@ -10,24 +10,47 @@ import SwiftUI
 
 struct LayerInspectorView: View {
     @State private var debugLocation: String = "none"
+//    @State private var nodesViewSpaceLocation: String = "none"
     
     var body: some View {
         
         VStack {
-            Text(self.debugLocation)
-                .border(.blue, width: 2)
-        }
-        .border(.red, width: 2)
-        .background {
-            GeometryReader { proxy in
-                Color.clear
-                    .onChange(of: proxy.frame(in: .named(GraphBaseView.coordinateNamespace)), initial: true) { _, newOrigin in
-                        self.debugLocation = newOrigin.debugDescription
+            Rectangle().fill(.cyan.opacity(0.8))
+                .frame(width: 100, height: 100)
+            //                .overlay {
+            //                    Text(self.debugLocation)
+            //                }
+            //                .border(.blue, width: 2)
+                .background {
+                    GeometryReader { proxy in
+                        Color.clear
+                        //                            .onChange(of: proxy.frame(in: .named(NodesView.coordinateNameSpace)),
+                            .onChange(of: proxy.frame(in: .global),
+                                      initial: true) { _, newFrame in
+                                log("LayerInspectorView: TEXT: onChangeOf frame: newFrame: \(newFrame)")
+                                self.debugLocation = newFrame.debugDescription
+                            }
                     }
-            }
+                    //                    .padding()
+                    .border(.blue, width: 6)
+                }
+            //                .padding()
+                .border(.red, width: 4)
+                .background {
+                    GeometryReader { proxy in
+//                        Color.clear
+                        Color.yellow.opacity(0.5)
+                        //                    .onChange(of: proxy.frame(in: .named(GraphBaseView.coordinateNamespace)),
+                            .onChange(of: proxy.frame(in: .named(NodesView.coordinateNameSpace)),
+                                      initial: true) { _, newFrame in
+                                log("LayerInspectorView: onChangeOf frame: newFrame: \(newFrame)")
+                                self.debugLocation = newFrame.debugDescription
+                            }
+                    }
+                }
+            //        .padding()
+                .border(.green, width: 2)
         }
-        .border(.green, width: 2)
-        
         
     }
 }
