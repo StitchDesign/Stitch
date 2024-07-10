3db23869493bc1f221440e119ff51d318cc07821
Chris Clampitt <christian.clampitt@nyu.edu>
2024-07-10 16:55:04 -0600

    cleanup

--- Stitch/Graph/PrototypePreview/Layer/View/CommonModifier/PreviewLayerSizeModifier.swift
+++ Stitch/Graph/PrototypePreview/Layer/View/CommonModifier/PreviewLayerSizeModifier.swift
@@ -22,13 +22,14 @@ extension CGFloat {
 // NOTE: it is the responsibility of the caller to make sure that sensible nil/non-nil params are passed in
 struct LayerSizeModifier: ViewModifier {
 
+    // TODO: non-frame-growing views like `Text`, `ProgressIndicator` etc. need .frame(width:height:alignment:) to be properly positioned with their frame; such views' dimennsions cannot be split up across separate `.frame(width:)`, `.frame(height:)` calls.
+    // Do you need an additional `pos: adjustPosition` modifier specifically for something like `Text` ?
     let alignment: Alignment
     
     let usesParentPercentForWidth: Bool
     let usesParentPercentForHeight: Bool
     
     // nil = dimension is unspecified
-    
     let width: CGFloat?
     let height: CGFloat?
         
@@ -37,69 +38,35 @@ struct LayerSizeModifier: ViewModifier {
     let minHeight: CGFloat?
     let maxHeight: CGFloat?
     
-//    var finalMinWidth: CGFloat? {
-//        // no matter what is passed in, if we are using parent-percent al
-//    }
-//    var finalMaxWidth: CGFloat? {
-//        // no matter what is passed in, if we are using parent-percent al
-//    }
-//    var finalMinHeight: CGFloat? {
-//        // no matter what is passed in, if we are using parent-percent al
-//    }
-//    var finalMaxHeight: CGFloat? {
-//        // no matter what is passed in, if we are using parent-percent al
-//    }
-//
-    
-//    let width: LayerDimension?
-//    let height: LayerDimension?
-//        
-//    let minWidth: LayerDimension?
-//    let maxWidth: LayerDimension?
-//    let minHeight: LayerDimension?
-//    let maxHeight: LayerDimension?
-    
     var someMinMaxDefined: Bool {
         minWidth.isDefined || maxWidth.isDefined || minHeight.isDefined || maxHeight.isDefined
     }
-    
-    // If user provided a pt  dimension (width or height),
-    // then we must use SwiftUI's `.frame(width:height:)`.
-    // Note that grow/hug for a dimension = `nil`, and so we can instead provide
-    var someDimensionFixed: Bool {
-        width.isDefined || height.isDefined
-    }
-            
-    
-    
+        
     func body(content: Content) -> some View {
-        logInView("LayerSizeModifier: width: \(width)")
-        logInView("LayerSizeModifier: height: \(height)")
-        logInView("LayerSizeModifier: minWidth: \(minWidth)")
-        logInView("LayerSizeModifier: maxWidth: \(maxWidth)")
-        logInView("LayerSizeModifier: minHeight: \(minHeight)")
-        logInView("LayerSizeModifier: maxHeight: \(maxHeight)")
-                
+        // logInView("LayerSizeModifier: width: \(width)")
+        // logInView("LayerSizeModifier: height: \(height)")
+        // logInView("LayerSizeModifier: minWidth: \(minWidth)")
+        // logInView("LayerSizeModifier: maxWidth: \(maxWidth)")
+        // logInView("LayerSizeModifier: minHeight: \(minHeight)")
+        // logInView("LayerSizeModifier: maxHeight: \(maxHeight)")
+               
+        // TODO: the below conditionals can be simplified, but are currently evolving; will be cleaned up after final iterations on conditional input logic
+        
         // Width is pt, but height is auto (so can use min/max height)
         if let width = width, !height.isDefined {
-            logInView("LayerSizeModifier: defined width but not height")
-            
-            // How to handle case where "set width, but non-nil min and max width passed down"
+            // logInView("LayerSizeModifier: defined width but not height")
             
             content
-            // parent percentage supports min/max along a dimension
+            // Note: parent-percentage supports min/max along a dimension
                 .frame(minWidth: usesParentPercentForWidth ? minWidth : nil)
                 .frame(maxWidth: usesParentPercentForWidth ? maxWidth : nil)
                 .frame(width: width)
                 .frame(minHeight: minHeight, maxHeight: maxHeight)
-            
-            // ^^ this will mess up the `alignment` for views like `Text`, `ProgressIndicator` etc.
-            // Do you need an additional `pos: adjustPosition` modifier specifically for something like `Text` ?
-        } 
+        }
         
         // Height is pt, but width is auto (so can use min/max width)
         else if let height = height, !width.isDefined {
-            logInView("LayerSizeModifier: defined height but not width")
+            // logInView("LayerSizeModifier: defined height but not width")
             
             content
                 .frame(minHeight: usesParentPercentForHeight ? minHeight : nil)
@@ -110,16 +77,12 @@ struct LayerSizeModifier: ViewModifier {
         
         // Both height and width are pt (so no min/max size at all)
         else if let width = width, let height = height {
-            logInView("LayerSizeModifier: defined width and height")
-            
+            // logInView("LayerSizeModifier: defined width and height")
             
             content
-//                .frame(width: width, height: height)
-            
                 .frame(minWidth: usesParentPercentForWidth ? minWidth : nil)
                 .frame(maxWidth: usesParentPercentForWidth ? maxWidth : nil)
                 .frame(width: width)
-            
                 .frame(minHeight: usesParentPercentForHeight ? minHeight : nil)
                 .frame(maxHeight: usesParentPercentForHeight ? maxHeight : nil)
                 .frame(height: height)
@@ -127,7 +90,7 @@ struct LayerSizeModifier: ViewModifier {
         
         // Both height and width are auto, so use min/max height and width
         else if someMinMaxDefined {
-            logInView("LayerSizeModifier: defined min-max")
+            // logInView("LayerSizeModifier: defined min-max")
             content.frame(minWidth: minWidth,
                           maxWidth: maxWidth,
                           minHeight: minHeight,
@@ -135,40 +98,12 @@ struct LayerSizeModifier: ViewModifier {
                           alignment: alignment)
         } 
         
-        // Default case
+        // Default
         else {
-            logInView("LayerSizeModifier: default")
+            // logInView("LayerSizeModifier: default")
             content.frame(width: width,
                           height: height,
                           alignment: alignment)
         }
-
-                
-//
-//        if someDimensionFixed {
-//            content
-//                .frame(width: width?.atleastZero(),
-//                       height: height?.atleastZero(),
-//                       alignment: alignment)
-//            
-//        } else {
-//                        
-//            // If we have at least one min-max dimension,
-//            // we cannot use the `.frame(width:height:)` API
-//            //        if someMinMaxDefined {
-//            content.frame(minWidth: minWidth?.atleastZero(),
-//                          maxWidth: maxWidth?.atleastOne(),
-//                          minHeight: minHeight?.atleastZero(),
-//                          maxHeight: maxHeight?.atleastOne(),
-//                          alignment: alignment)
-//        }
-//        
-        
-        //        } else {
-        //            content
-        //            .frame(width: width?.atleastZero(),
-        //                          height: height?.atleastZero(),
-        //                          alignment: alignment)
-        //        }
     }
 }
