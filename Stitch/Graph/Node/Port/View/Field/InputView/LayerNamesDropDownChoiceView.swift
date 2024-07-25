0df1eaf05197954e02bdb108781ace9b1c87a3ca
Chris Clampitt <christian.clampitt@nyu.edu>
2024-07-25 14:59:17 -0600

    Do not use StitchMenu with LayerNamesDropDownChoiceView

--- Stitch/Graph/Node/Port/View/Field/InputView/LayerNamesDropDownChoiceView.swift
+++ Stitch/Graph/Node/Port/View/Field/InputView/LayerNamesDropDownChoiceView.swift
@@ -10,7 +10,7 @@ import StitchSchemaKit
 import SwiftUI
 import OrderedCollections
 
-// TODO: can this be combined with regular Dropdown view?
+// Note:
 struct LayerNamesDropDownChoiceView: View {
     @State private var selection: NodeViewModel = NodeViewModel.nilChoice
 
@@ -44,67 +44,40 @@ struct LayerNamesDropDownChoiceView: View {
     }
 
     var body: some View {
-        StitchMenu(id: id.nodeId,
-                   selection: selectionTitle,
-                   contentCatalyst: {
-                    // TODO: replace with the string-based Picker of DropdownChoiceView + StitchPickerView ? Any reason why you can't use a SwiftUI Picker here? Something about key presses?
-                    ForEach(self.layerOptions) { node in
-                        @Bindable var node = node
-                        StitchButton {
-                            self.onSet(node)
-                        } label: {
-                            #if DEV_DEBUG
-                            StitchTextView(string: "\(node.displayTitle) \(node.id.description.dropLast(24))")
-                            #else
-                            StitchTextView(string: node.displayTitle)
-                            #endif
-                        }
-                    }
-                   }, contentIPad: {
-                       // TODO: revisit this; may want to use the string-based Picker of DropdownChoiceView + StitchPickerView; until we do so, or update the below code, LayerNamesDropDown on iPad will not properly show the picker
-                       // see https://github.com/vpl-codesign/stitch/issues/5294
-                       
-//                       Picker("", selection: self.$selection) {
-//                           ForEach(self.layerOptions) { node in
-//                               @Bindable var node = node
-//                               StitchTextView(string: node.displayTitle)
-//                               // MARK: tag necessaray to get this to work
-//                               //                                .tag(node)
-//                                   .tag(node.id)
-//                           }
-//                       } // Picker
-                       
-                       ForEach(self.layerOptions) { node in
-                           @Bindable var node = node
-                           StitchButton {
-                               self.onSet(node)
-                           } label: {
-                               #if DEV_DEBUG
-                               StitchTextView(string: "\(node.displayTitle) \(node.id.description.dropLast(24))")
-                               #else
-                               StitchTextView(string: node.displayTitle)
-                               #endif
-                           }
-                       }
-                       
-                   }, // contentIPad:
-                   
-                   // overal label for the view
-                   label: {
-                       StitchTextView(string: selectionTitle)
-                   }
-        )
-            .onChange(of: self.selection.id) {
-                self.onSet(self.selection)
-            }
-            .onChange(of: self.value, initial: true) {
-                guard let persistedInteractionId = value.getInteractionId,
-                      let node = self.graph.getNodeViewModel(persistedInteractionId.id) else {
-                    self.selection = NodeViewModel.nilChoice
-                    return
+        
+        Menu {
+            ForEach(self.layerOptions) { node in
+                @Bindable var node = node
+                StitchButton {
+                    self.onSet(node)
+                } label: {
+#if DEV_DEBUG
+                    StitchTextView(string: "\(node.displayTitle) \(node.id.description.dropLast(24))")
+#else
+                    StitchTextView(string: node.displayTitle)
+#endif
                 }
-
-                self.selection = node
             }
+        } label: {
+            StitchTextView(string: selectionTitle)
+        }
+#if targetEnvironment(macCatalyst)
+        .buttonStyle(.plain)
+#endif
+        
+        
+        
+        .onChange(of: self.selection.id) {
+            self.onSet(self.selection)
+        }
+        .onChange(of: self.value, initial: true) {
+            guard let persistedInteractionId = value.getInteractionId,
+                  let node = self.graph.getNodeViewModel(persistedInteractionId.id) else {
+                self.selection = NodeViewModel.nilChoice
+                return
+            }
+            
+            self.selection = node
+        }
     }
 }
