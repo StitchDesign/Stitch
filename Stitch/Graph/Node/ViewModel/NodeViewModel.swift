fc5d63e530afb8b8b632a7ff8041b224417dc092
Chris Clampitt <christian.clampitt@nyu.edu>
2024-08-05 13:49:40 -0600

    comments on hasLoop

--- Stitch/Graph/Node/ViewModel/NodeViewModel.swift
+++ Stitch/Graph/Node/ViewModel/NodeViewModel.swift
@@ -265,6 +265,8 @@ extension NodeViewModel {
     }
     
     // THIS METHOD ONLY USED WHEN A NODE COMES BACK ON-SCREEN ?
+    // i.e. not a perf sensitive case
+    // Also, visibility status change DOES NOT affect looped-values etc. 
     @MainActor
     func updateInputPortViewModels(activeIndex: ActiveIndex) {
         zip(self.getAllInputsObservers(), self.inputs).forEach { rowObserver, values in
