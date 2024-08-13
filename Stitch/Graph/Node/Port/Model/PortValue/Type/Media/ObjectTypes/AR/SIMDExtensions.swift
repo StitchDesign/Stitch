0a7e1e84b96826d2ebc8e004ecd8b26de90b65e6
Nick Arner <nicholasarner@gmail.com>
2024-08-12 18:07:58 -0700

    wip

--- Stitch/Graph/Node/Port/Model/PortValue/Type/Media/ObjectTypes/AR/SIMDExtensions.swift
+++ Stitch/Graph/Node/Port/Model/PortValue/Type/Media/ObjectTypes/AR/SIMDExtensions.swift
@@ -30,53 +30,6 @@ extension simd_quatf {
     }
 }
 
-extension simd_float3x3 {
-    // Normalizes each column to unit scale
-    func normalized() -> simd_float3x3 {
-        return simd_float3x3(simd.normalize(columns.0),
-                             simd.normalize(columns.1),
-                             simd.normalize(columns.2))
-    }
-
-    var eulerAngles: SIMD3<Float> {
-        let M = self.normalized()
-
-        let R11 = M.columns.0.x
-        let R12 = M.columns.1.x
-        let R13 = M.columns.2.x
-
-        let R21 = M.columns.0.y
-        let R22 = M.columns.1.y
-        let R23 = M.columns.2.y
-
-        let R31 = M.columns.0.z
-        let R32 = M.columns.1.z
-        let R33 = M.columns.2.z
-
-        if abs(R31) != 1.0 {
-            let t1 = -asin(R31)
-            let t2 = .pi - t1
-            let c1 = cos(t1)
-            let c2 = cos(t2)
-            let psi1 = atan2(R32 / c1, R33 / c1)
-            let psi2 = atan2(R32 / c2, R33 / c2)
-            let phi1 = atan2(R21 / c1, R11 / c1)
-            let phi2 = atan2(R21 / c2, R11 / c2)
-            return SIMD3<Float>(psi1, t1, phi1)
-        } else {
-            let phi: Float = 0.0 // arbitrary
-            if R31 == -1 {
-                let t = Float.pi / 2
-                let psi = phi + atan2(R12, R13)
-                return SIMD3<Float>(psi, t, phi)
-            } else {
-                let t = -Float.pi / 2
-                let psi = -phi + atan2(-R12, -R13)
-                return SIMD3<Float>(psi, t, phi)
-            }
-        }
-    }
-}
 
 
 // Extension to create a 4x4 rotation matrix from Euler angles
@@ -133,23 +86,6 @@ extension simd_float4x4 {
             SIMD3<Float>(columns.2.x, columns.2.y, columns.2.z) / scale.z
         )
     }
-    
-    var rotationX: SIMD3<Float> {
-        return SIMD3<Float>(rotationMatrix[0][0], rotationMatrix[1][0], rotationMatrix[2][0])
-    }
-
-    var rotationY: SIMD3<Float> {
-        return SIMD3<Float>(rotationMatrix[0][1], rotationMatrix[1][1], rotationMatrix[2][1])
-    }
-
-    var rotationZ: SIMD3<Float> {
-        return SIMD3<Float>(rotationMatrix[0][2], rotationMatrix[1][2], rotationMatrix[2][2])
-    }
-    
-    var orientation: simd_quatf {
-        simd_quaternion(self)
-    }
-
     // Extract Euler angles (in radians) from the matrix
     var eulerAngles: SIMD3<Float> {
         let rotMatrix = rotationMatrix
@@ -175,6 +111,26 @@ extension simd_float4x4 {
         return angles
     }
 
+    
+    
+    
+    var rotationX: SIMD3<Float> {
+        return SIMD3<Float>(rotationMatrix[0][0], rotationMatrix[1][0], rotationMatrix[2][0])
+    }
+
+    var rotationY: SIMD3<Float> {
+        return SIMD3<Float>(rotationMatrix[0][1], rotationMatrix[1][1], rotationMatrix[2][1])
+    }
+
+    var rotationZ: SIMD3<Float> {
+        return SIMD3<Float>(rotationMatrix[0][2], rotationMatrix[1][2], rotationMatrix[2][2])
+    }
+    
+    var orientation: simd_quatf {
+        simd_quaternion(self)
+    }
+
+
     // New method to extract rotation in radians
     var rotationInRadians: SIMD3<Float> {
         let rotMatrix = rotationMatrix
@@ -194,6 +150,7 @@ extension simd_float4x4 {
         
         return angles
     }
+
 
     // Convert Euler angles to degrees
     var eulerAnglesDegrees: SIMD3<Float> {
