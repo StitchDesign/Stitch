5f73b4863bf90e3ba5542a76864b409d64f66180
Nick Arner <nicholasarner@gmail.com>
2024-08-15 17:36:27 -0700

    updating node value type

--- Stitch/Graph/Node/Patch/Type/Pack/TransformPackNode.swift
+++ Stitch/Graph/Node/Patch/Type/Pack/TransformPackNode.swift
@@ -13,33 +13,33 @@ struct TransformPackPatchNode: PatchNodeDefinition {
     static let patch = Patch.transformPack
 
     static func rowDefinitions(for type: UserVisibleType?) -> NodeRowDefinitions {
-        let inputTransform = DEFAULT_TRANSFORM_MATRIX
+        let inputTransform = DEFAULT_STITCH_TRANSFORM
         return NodeRowDefinitions(inputs: [
-            .init(defaultValues: [.number(Double(inputTransform.position.x))],
+            .init(defaultValues: [.number(Double(inputTransform.positionX))],
                   label: "Position X",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.position.y))],
+            .init(defaultValues: [.number(Double(inputTransform.positionY))],
                   label: "Position Y",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.position.z))],
+            .init(defaultValues: [.number(Double(inputTransform.positionZ))],
                   label: "Position Z",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.scale.x))],
+            .init(defaultValues: [.number(Double(inputTransform.scaleX))],
                   label: "Scale X",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.scale.y))],
+            .init(defaultValues: [.number(Double(inputTransform.scaleY))],
                   label: "Scale Y",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.scale.z))],
+            .init(defaultValues: [.number(Double(inputTransform.scaleZ))],
                   label: "Scale Z",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.rotation.imag.x))],
+            .init(defaultValues: [.number(Double(inputTransform.rotationX))],
                   label: "Rotation X",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.rotation.imag.y))],
+            .init(defaultValues: [.number(Double(inputTransform.rotationY))],
                   label: "Rotation Y",
                   isTypeStatic: true),
-            .init(defaultValues: [.number(Double(inputTransform.rotation.imag.z))],
+            .init(defaultValues: [.number(Double(inputTransform.rotationZ))],
                   label: "Rotation Z",
                   isTypeStatic: true)
         ],
