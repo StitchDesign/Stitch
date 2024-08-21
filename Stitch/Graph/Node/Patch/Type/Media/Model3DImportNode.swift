9f267825b21d0d9df203da8e43d13e5f874dab82
Nick Arner <nicholasarner@gmail.com>
2024-08-20 18:30:16 -0700

    LOL TYPES

--- Stitch/Graph/Node/Patch/Type/Media/Model3DImportNode.swift
+++ Stitch/Graph/Node/Patch/Type/Media/Model3DImportNode.swift
@@ -60,38 +60,55 @@ func model3DImportEval(node: PatchNode) -> EvalResult {
         let transform = values[Model3DImportNodeIndices.matrix].getTransform ?? StitchTransform()
         let matrix: matrix_float4x4 = matrix_float4x4(from: transform)
         
-        let model3DEntity = media.mediaObject.model3DEntity
-                
-        // Update transform
-//        let rotationX = model3DEntity?.originalTransformMatrix?.rotationInRadians.x
-//        let rotationY = model3DEntity?.originalTransformMatrix?.rotationInRadians.y
-//        let rotationZ = model3DEntity?.originalTransformMatrix?.rotationInRadians.z
-
+        guard let model3DEntity = media.mediaObject.model3DEntity else {
+            return node.defaultOutputs
+        }
         
         //transform is empty, so use the original transform value
         if transform == DEFAULT_STITCH_TRANSFORM {
+            let position: simd_float3
+            if let originalPosition = model3DEntity.originalTransformMatrix?.position {
+                position = simd_float3(originalPosition)
+            } else {
+                position = simd_float3(0.0, 0.0, 0.0)
+            }
             
-            let position = model3DEntity?.originalTransformMatrix?.position
-            let scale = model3DEntity?.originalTransformMatrix?.scale
+            let scale: simd_float3
+            if let originalScale = model3DEntity.originalTransformMatrix?.scale {
+                scale = simd_float3(originalScale)
+            } else {
+                scale = simd_float3(1.0, 1.0, 1.0)
+            }
 
-                    let rotationX = model3DEntity?.originalTransformMatrix?.rotationInRadians.x
-                    let rotationY = model3DEntity?.originalTransformMatrix?.rotationInRadians.y
-                    let rotationZ = model3DEntity?.originalTransformMatrix?.rotationInRadians.z
+            let rotationX = Double(model3DEntity.originalTransformMatrix?.rotationInRadians.x ?? 0.0)
+            let rotationY = Double(model3DEntity.originalTransformMatrix?.rotationInRadians.y ?? 0.0)
+            let rotationZ = Double(model3DEntity.originalTransformMatrix?.rotationInRadians.z ?? 0.0)
 
-            
-//            transform = StitchTransform(positionX: position?.x, positionY: position?.y, positionZ: position?.z, scaleX: scale?.x, scaleY: scale?.y, scaleZ: scale?.z, rotationX: 0, rotationY: 0, rotationZ: 0)
+            transform = StitchTransform(
+                positionX: Double(position.x),
+                positionY: Double(position.y),
+                positionZ: Double(position.z),
+                scaleX: Double(scale.x),
+                scaleY: Double(scale.y),
+                scaleZ: Double(scale.z),
+                rotationX: rotationX,
+                rotationY: rotationY,
+                rotationZ: rotationZ
+            )
         }
-        
-        let matrix: matrix_float4x4 = matrix_float4x4(position: simd_float3(Float(transform.positionX), Float(transform.positionY), Float(transform.positionZ)), scale: simd_float3(Float(transform.scaleX), Float(transform.scaleY), Float(transform.scaleZ)), rotationZYX: simd_float3(Float(transform.rotationX), Float(transform.rotationY), Float(transform.rotationZ)))
 
+        let position = simd_float3(Float(transform.positionX), Float(transform.positionY), Float(transform.positionZ))
+        let scale = simd_float3(Float(transform.scaleX), Float(transform.scaleY), Float(transform.scaleZ))
+        let rotationZYX = simd_float3(Float(transform.rotationX), Float(transform.rotationY), Float(transform.rotationZ))
         
-        model3DEntity?.applyMatrix(newMatrix: matrix)
+        let matrix = matrix_float4x4(position: position, scale: scale, rotationZYX: rotationZYX)
         
-        switch model3DEntity?.entityStatus {
+        model3DEntity.applyMatrix(newMatrix: matrix)
+        
+        switch model3DEntity.entityStatus {
         case .loaded(let entity):
             // Update animation
-            model3DEntity?.isAnimating = animating
-            
+            model3DEntity.isAnimating = animating
             return [media.portValue]
         default:
             // Return nil object if 3D model still loading or none
@@ -104,4 +121,20 @@ struct Model3DImportNodeIndices {
     static let media = 0
     static let animating = 1
     static let matrix = 2
+}
+
+
+import SceneKit
+import simd
+
+extension simd_float3 {
+    init(_ vector: SCNVector3) {
+        self.init(x: vector.x, y: vector.y, z: vector.z)
+    }
+}
+
+extension SCNVector3 {
+    init(_ vector: simd_float3) {
+        self.init(x: vector.x, y: vector.y, z: vector.z)
+    }
 }
