d23d6f885e755b1ec54a42bb29e2a66465031a84
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-10-02 19:25:52 -0700

    Commented out perf unit tests

--- StitchTests/perfTests.swift
+++ StitchTests/perfTests.swift
@@ -7,42 +7,42 @@
 
 import XCTest
 
-final class perfTests: XCTestCase {
-
-    func testNumberFormatting() throws {
-
-        let fmt = NumberFormatter()
-        fmt.numberStyle = .decimal
-        fmt.maximumFractionDigits = 4
-
-        let xs = Array.init(repeating: "-0.0",
-                            count: 8000)
-
-        // 0.000919, 0.000887
-        self.measure {
-            xs.forEach { x in
-                fmt.string(for: x)
-            }
-        }
-    }
-
-    func testNumberDescription() throws {
-
-        let fmt = NumberFormatter()
-        fmt.numberStyle = .decimal
-        // stops usage of comma separator which creates issues with editing later
-        //        fmt.groupingSeparator = .empty
-        fmt.maximumFractionDigits = 4
-
-        let xs = Array.init(repeating: "-0.0",
-                            count: 8000)
-
-        // 0.00733, 0.000545, 0.000548
-        self.measure {
-            xs.forEach { x in
-                x.description
-            }
-        }
-    }
-
-}
+//final class perfTests: XCTestCase {
+//
+//    func testNumberFormatting() throws {
+//
+//        let fmt = NumberFormatter()
+//        fmt.numberStyle = .decimal
+//        fmt.maximumFractionDigits = 4
+//
+//        let xs = Array.init(repeating: "-0.0",
+//                            count: 8000)
+//
+//        // 0.000919, 0.000887
+//        self.measure {
+//            xs.forEach { x in
+//                fmt.string(for: x)
+//            }
+//        }
+//    }
+//
+//    func testNumberDescription() throws {
+//
+//        let fmt = NumberFormatter()
+//        fmt.numberStyle = .decimal
+//        // stops usage of comma separator which creates issues with editing later
+//        //        fmt.groupingSeparator = .empty
+//        fmt.maximumFractionDigits = 4
+//
+//        let xs = Array.init(repeating: "-0.0",
+//                            count: 8000)
+//
+//        // 0.00733, 0.000545, 0.000548
+//        self.measure {
+//            xs.forEach { x in
+//                x.description
+//            }
+//        }
+//    }
+//
+//}
