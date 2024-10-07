4c868799ebbd2df5f6266e53d7e7ecb4d4f36c14
Chris Clampitt <christian.clampitt@nyu.edu>
2024-10-07 11:06:56 -0600

    Shortcuts to add Wireless Broadcaster and Receiver to graph (#382)

--- Stitch/App/Shortcut/ProjectsHomeCommands.swift
+++ Stitch/App/Shortcut/ProjectsHomeCommands.swift
@@ -162,6 +162,27 @@ struct ProjectsHomeCommands: Commands {
                                     disabled: textFieldFocused) {
                     dispatch(NodeCreatedEvent(choice: .patch(.sizePack)))
                 }
+                
+                // Option + W = add Broadcaster
+                SwiftUIShortcutView(title: "Insert Wireless Broadcaster",
+                                    key: ADD_WIRELESS_NODE_SHORTCUT,
+                                    eventModifiers: [.option],
+                                    disabled: textFieldFocused) {
+                    dispatch(NodeCreatedEvent(choice: .patch(.wirelessBroadcaster)))
+                    // TODO: probably not needed?
+                    store.currentDocument?.keypressState.modifiers.remove(.option)
+                }
+                
+                // Option + Shift + W = add Receiver
+                SwiftUIShortcutView(title: "Insert Wireless Receiver",
+                                    key: ADD_WIRELESS_NODE_SHORTCUT,
+                                    eventModifiers: [.option, .shift],
+                                    disabled: textFieldFocused) {
+                    dispatch(NodeCreatedEvent(choice: .patch(.wirelessReceiver)))
+                    // Note: the Option key seems to get stuck easily when Shift is also pressed?
+                    store.currentDocument?.keypressState.modifiers.remove(.option)
+                    store.currentDocument?.keypressState.modifiers.remove(.shift)
+                }
             } // if activeProject
 
         } // CommandMenu
@@ -300,9 +321,6 @@ struct ProjectsHomeCommands: Commands {
                 }
                 
             } // replacing: .pasteboard
-            
-            
-            
             
         } // if activeProject
     }
