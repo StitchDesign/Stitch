41f2e37a7e94c2dbcffdb250afad172da1a2eee0
Chris Clampitt <christian.clampitt@nyu.edu>
2024-10-16 09:59:33 -0600

    Right click option to hide layer; fix layer hiding via node tag menu; visually indicate hidden layer items on canvas (#443)

--- Stitch/Graph/Sidebar/Util/Gesture/SidebarListItemActions.swift
+++ Stitch/Graph/Sidebar/Util/Gesture/SidebarListItemActions.swift
@@ -41,28 +41,51 @@ struct SidebarItemHiddenStatusToggled: GraphEventWithResponse {
 
     let clickedId: LayerNodeId
 
+    @MainActor
     func handle(state: GraphState) -> GraphResponse {
+        state.layerHiddenStatusToggled(clickedId)
+        return .persistenceResponse
+    }
+}
 
-        let sidebarGroups = state.getSidebarGroupsDict()
+// TODO: how to handle 'toggling' hidden status when multiple layers are selected and each layer may already have its own hidden status? ... Should user explicitly pick "Hide Layer" vs "Unhide Layer"?
+struct SelectedLayersHiddenStatusToggled: GraphEventWithResponse {
+
+    let selectedLayers: LayerIdSet
+    
+    @MainActor
+    func handle(state: GraphState) -> GraphResponse {
+        // Careful -- some of these layers may be descendants of each other, and we currently toggle the descendants' status as well
+        for selectedLayer in selectedLayers {
+            state.layerHiddenStatusToggled(selectedLayer)
+        }
+        return .persistenceResponse
+    }
+}
+
+extension GraphState {
+    @MainActor
+    func layerHiddenStatusToggled(_ clickedId: LayerNodeId) {
+        
+        guard let layerNode = self.getLayerNode(id: clickedId.id)?.layerNode else {
+            log("SidebarItemHiddenStatusToggled: could not find layer node for clickedId \(clickedId.id)")
+            fatalErrorIfDebug() // Is this bad?
+            return
+        }
+        
+        let sidebarGroups = self.getSidebarGroupsDict()
         
         let descendants: LayerIdSet = getDescendantsIds(
             id: clickedId,
             groups: sidebarGroups,
             acc: LayerIdSet())
 
-        guard let layerNode = state.getLayerNode(id: clickedId.id)?.layerNode else {
-            log("SidebarItemHiddenStatusToggled: could not find layer node for clickedId \(clickedId.id)")
-            return .noChange
-        }
-                
         layerNode.hasSidebarVisibility.toggle()
         
         let isShown = layerNode.hasSidebarVisibility
         
         for id in descendants {
-            state.getLayerNode(id: id.id)?.layerNode?.hasSidebarVisibility = isShown
+            self.getLayerNode(id: id.id)?.layerNode?.hasSidebarVisibility = isShown
         }
-        
-        return .persistenceResponse
     }
 }
