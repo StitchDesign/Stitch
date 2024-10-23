16f93e7f2b4b4e500b61eb865bb9da5a577bf625
Chris Clampitt <christian.clampitt@nyu.edu>
2024-10-23 11:24:09 -0600

    Add sizing, position and anchoring to gradient layers; subfolders for layers (#466)

--- Stitch/Graph/Node/Layer/Type/Gradients/LinearGradientNode.swift
+++ Stitch/Graph/Node/Layer/Type/Gradients/LinearGradientNode.swift
@@ -20,15 +20,23 @@ struct LinearGradientLayerNode: LayerNodeDefinition {
     
     static let inputDefinitions: LayerInputTypeSet = .init([
         .enabled,
+        .position,
+        .size,
         .opacity,
         .scale,
+        .anchoring,
         .zIndex,
         .startAnchor,
         .endAnchor,
         .startColor,
         .endColor
     ])
-        .union(.layerEffects).union(.pinning).union(.layerPaddingAndMargin).union(.offsetInGroup)
+        .union(.layerEffects)
+        .union(.aspectRatio)
+        .union(.sizing)
+        .union(.pinning)
+        .union(.layerPaddingAndMargin)
+        .union(.offsetInGroup)
     
     
     static func content(document: StitchDocumentViewModel,
@@ -44,9 +52,12 @@ struct LinearGradientLayerNode: LayerNodeDefinition {
             layerViewModel: viewModel,
             isPinnedViewRendering: isPinnedViewRendering,
             interactiveLayer: viewModel.interactiveLayer,
-            enabled: viewModel.enabled.getBool ?? true, 
+            enabled: viewModel.enabled.getBool ?? true,
+            position: viewModel.position.getPosition ?? .zero,
+            size: viewModel.size.getSize ?? .zero,
             opacity: viewModel.opacity.getNumber ?? defaultOpacityNumber,
             scale: viewModel.scale.getNumber ?? 1,
+            anchoring: viewModel.anchoring.getAnchoring ?? .defaultAnchoring,
             blurRadius: viewModel.blurRadius.getNumber ?? .zero,
             blendMode: viewModel.blendMode.getBlendMode ?? .defaultBlendMode,
             brightness: viewModel.brightness.getNumber ?? .defaultBrightnessForLayerEffect,
@@ -59,8 +70,7 @@ struct LinearGradientLayerNode: LayerNodeDefinition {
             firstColor: viewModel.startColor.getColor ?? .yellow,
             secondColor: viewModel.endColor.getColor ?? .blue,
             parentSize: parentSize,
-            // Must this always be true?
-            parentDisablesPosition: true)
+            parentDisablesPosition: parentDisablesPosition)
     }
 }
 
@@ -72,8 +82,11 @@ struct PreviewLinearGradientLayer: View {
     let isPinnedViewRendering: Bool
     let interactiveLayer: InteractiveLayer
     let enabled: Bool
+    let position: CGPoint
+    let size: LayerSize
     let opacity: Double
     let scale: Double
+    let anchoring: Anchoring
     let blurRadius: CGFloat
     let blendMode: StitchBlendMode
     let brightness: Double
@@ -87,45 +100,40 @@ struct PreviewLinearGradientLayer: View {
     let secondColor: Color
     let parentSize: CGSize
     let parentDisablesPosition: Bool
-    let position: CGPoint = .zero
-
-    var size: LayerSize {
-        parentSize.toLayerSize
-    }
 
     var body: some View {
 
-        return LinearGradient(colors: [firstColor, secondColor], 
-                              startPoint: startPoint.toUnitPointType,
-                              endPoint: endPoint.toUnitPointType)
-            .opacity(enabled ? opacity : 0.0)
-            .modifier(PreviewCommonModifier(
-                document: document,
-                graph: graph,
-                layerViewModel: layerViewModel,
-                isPinnedViewRendering: isPinnedViewRendering,
-                interactiveLayer: interactiveLayer,
-                position: position,
-                rotationX: .zero,
-                rotationY: .zero,
-                rotationZ: .zero,
-                size: size,
-                scale: scale,
-                anchoring: .defaultAnchoring,
-                blurRadius: blurRadius,
-                blendMode: blendMode,
-                brightness: brightness,
-                colorInvert: colorInvert,
-                contrast: contrast,
-                hueRotation: hueRotation,
-                saturation: saturation,
-                pivot: .defaultPivot,
-                shadowColor: .defaultShadowColor,
-                shadowOpacity: .defaultShadowOpacity,
-                shadowRadius: .defaultShadowRadius,
-                shadowOffset: .defaultShadowOffset,
-                parentSize: parentSize,
-                parentDisablesPosition: parentDisablesPosition))
+        LinearGradient(colors: [firstColor, secondColor],
+                       startPoint: startPoint.toUnitPointType,
+                       endPoint: endPoint.toUnitPointType)
+        .opacity(enabled ? opacity : 0.0)
+        .modifier(PreviewCommonModifier(
+            document: document,
+            graph: graph,
+            layerViewModel: layerViewModel,
+            isPinnedViewRendering: isPinnedViewRendering,
+            interactiveLayer: interactiveLayer,
+            position: position,
+            rotationX: .zero,
+            rotationY: .zero,
+            rotationZ: .zero,
+            size: size,
+            scale: scale,
+            anchoring: anchoring,
+            blurRadius: blurRadius,
+            blendMode: blendMode,
+            brightness: brightness,
+            colorInvert: colorInvert,
+            contrast: contrast,
+            hueRotation: hueRotation,
+            saturation: saturation,
+            pivot: .defaultPivot,
+            shadowColor: .defaultShadowColor,
+            shadowOpacity: .defaultShadowOpacity,
+            shadowRadius: .defaultShadowRadius,
+            shadowOffset: .defaultShadowOffset,
+            parentSize: parentSize,
+            parentDisablesPosition: parentDisablesPosition))
     }
 }
 
