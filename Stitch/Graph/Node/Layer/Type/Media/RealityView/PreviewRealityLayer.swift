05a9875926880a253c7c7a4731bd1a61c3f23877
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-10-31 13:06:33 -0700

    Reality node support for Mac (#514)

--- Stitch/Graph/Node/Layer/Type/Media/RealityView/PreviewRealityLayer.swift
+++ Stitch/Graph/Node/Layer/Type/Media/RealityView/PreviewRealityLayer.swift
@@ -19,7 +19,6 @@ struct PreviewRealityLayer: View {
     let parentDisablesPosition: Bool
     
     var body: some View {
-        let nodeId = self.viewModel.id.layerNodeId.id
         let position = viewModel.position.getPosition ?? .zero
         let rotationX = viewModel.rotationX.asCGFloat
         let rotationY = viewModel.rotationY.asCGFloat
@@ -29,67 +28,48 @@ struct PreviewRealityLayer: View {
         let anchoring = viewModel.anchoring.getAnchoring ?? .defaultAnchoring
         let scale = viewModel.scale.asCGFloat
         
-        switch document.cameraFeedManager {
-        case .loaded(let cameraFeedManager):
-            if let cameraFeedManager = cameraFeedManager.cameraFeedManager,
-               let node = document.visibleGraph.getNodeViewModel(viewModel.id.layerNodeId.asNodeId) {
-                @Bindable var node = node
-                
-                RealityLayerView(document: document,
-                                 graph: graph,
-                                 node: node,
-                                 layerViewModel: viewModel,
-                                 cameraFeedManager: cameraFeedManager, 
-                                 isPinnedViewRendering: isPinnedViewRendering,
-                                 interactiveLayer: self.viewModel.interactiveLayer,
-                                 allAnchors: viewModel.allAnchors.compactMap { $0.asyncMedia },
-                                 position: position,
-                                 rotationX: rotationX,
-                                 rotationY: rotationY,
-                                 rotationZ: rotationZ,
-                                 size: size,
-                                 layerSize: layerSize,
-                                 scale: scale,
-                                 opacity: viewModel.opacity.asCGFloat,
-                                 anchoring: anchoring,
-                                 isCameraFeedEnabled: viewModel.isCameraEnabled.getBool ?? false,
-                                 isShadowsEnabled: viewModel.isShadowsEnabled.getBool ?? false,
-                                 blurRadius: viewModel.blurRadius.getNumber ?? .zero,
-                                 blendMode: viewModel.blendMode.getBlendMode ?? .defaultBlendMode,
-                                 brightness: viewModel.brightness.getNumber ?? .defaultBrightnessForLayerEffect,
-                                 colorInvert: viewModel.colorInvert.getBool ?? .defaultColorInvertForLayerEffect,
-                                 contrast: viewModel.contrast.getNumber ?? .defaultContrastForLayerEffect,
-                                 hueRotation: viewModel.hueRotation.getNumber ?? .defaultHueRotationForLayerEffect,
-                                 saturation: viewModel.saturation.getNumber ?? .defaultSaturationForLayerEffect,
-                                 pivot: viewModel.pivot.getAnchoring ?? .defaultPivot,
-                                 shadowColor: viewModel.shadowColor.getColor ?? .defaultShadowColor,
-                                 shadowOpacity: viewModel.shadowOpacity.getNumber ?? .defaultShadowOpacity,
-                                 shadowRadius: viewModel.shadowRadius.getNumber ?? .defaultShadowOpacity,
-                                 shadowOffset: viewModel.shadowOffset.getPosition ?? .defaultShadowOffset,
-                                 parentSize: parentSize,
-                                 parentDisablesPosition: parentDisablesPosition)
-                .onAppear {
-                    // Update list of node Ids using camera
-                    graph.enabledCameraNodeIds.insert(nodeId)
-                }
-            } else {
-                EmptyView()
-                    .onAppear {
-#if DEBUG
-                        fatalError()
-#endif
-                    }
-            }
+        if let node = document.visibleGraph.getNodeViewModel(viewModel.id.layerNodeId.asNodeId) {
+            @Bindable var node = node
             
-        case .loading, .failed:
+            RealityLayerView(document: document,
+                             graph: graph,
+                             node: node,
+                             layerViewModel: viewModel,
+                             cameraFeedManager: document.cameraFeedManager?.loadedInstance?.cameraFeedManager,
+                             isPinnedViewRendering: isPinnedViewRendering,
+                             interactiveLayer: self.viewModel.interactiveLayer,
+                             allAnchors: viewModel.allAnchors.compactMap { $0.asyncMedia },
+                             position: position,
+                             rotationX: rotationX,
+                             rotationY: rotationY,
+                             rotationZ: rotationZ,
+                             size: size,
+                             layerSize: layerSize,
+                             scale: scale,
+                             opacity: viewModel.opacity.asCGFloat,
+                             anchoring: anchoring,
+                             isCameraFeedEnabled: viewModel.isCameraEnabled.getBool ?? false,
+                             isShadowsEnabled: viewModel.isShadowsEnabled.getBool ?? false,
+                             blurRadius: viewModel.blurRadius.getNumber ?? .zero,
+                             blendMode: viewModel.blendMode.getBlendMode ?? .defaultBlendMode,
+                             brightness: viewModel.brightness.getNumber ?? .defaultBrightnessForLayerEffect,
+                             colorInvert: viewModel.colorInvert.getBool ?? .defaultColorInvertForLayerEffect,
+                             contrast: viewModel.contrast.getNumber ?? .defaultContrastForLayerEffect,
+                             hueRotation: viewModel.hueRotation.getNumber ?? .defaultHueRotationForLayerEffect,
+                             saturation: viewModel.saturation.getNumber ?? .defaultSaturationForLayerEffect,
+                             pivot: viewModel.pivot.getAnchoring ?? .defaultPivot,
+                             shadowColor: viewModel.shadowColor.getColor ?? .defaultShadowColor,
+                             shadowOpacity: viewModel.shadowOpacity.getNumber ?? .defaultShadowOpacity,
+                             shadowRadius: viewModel.shadowRadius.getNumber ?? .defaultShadowOpacity,
+                             shadowOffset: viewModel.shadowOffset.getPosition ?? .defaultShadowOffset,
+                             parentSize: parentSize,
+                             parentDisablesPosition: parentDisablesPosition)
+        } else {
             EmptyView()
-            
-        case .none:
-            // Note that EmptyView won't trigger the onApppear closure
-            Color.clear
                 .onAppear {
-                    document.realityViewCreatedWithoutCamera(graph: graph,
-                                                             nodeId: nodeId)
+#if DEBUG
+                    fatalError()
+#endif
                 }
         }
     }
@@ -101,7 +81,7 @@ struct RealityLayerView: View {
     @Bindable var node: NodeViewModel
     let layerViewModel: LayerViewModel
     
-    let cameraFeedManager: CameraFeedManager
+    let cameraFeedManager: CameraFeedManager?
     let isPinnedViewRendering: Bool
     let interactiveLayer: InteractiveLayer
     let allAnchors: [GraphMediaValue]
@@ -133,25 +113,66 @@ struct RealityLayerView: View {
     let parentSize: CGSize
     let parentDisablesPosition: Bool
     
+    // Override camera setting on Mac
+    var _isCameraEnabled: Bool {
+#if targetEnvironment(macCatalyst)
+        return false
+#else
+        return isCameraFeedEnabled
+#endif
+    }
+    
     var body: some View {
         Group {
-            if isPinnedViewRendering, // Can't run multiple reality views
-               let arView = cameraFeedManager.arView,
-               !document.isGeneratingProjectThumbnail {
-                RealityView(arView: arView,
-                            size: layerSize,
-                            scale: scale,
-                            opacity: opacity,
-                            isCameraEnabled: isCameraFeedEnabled,
-                            isShadowsEnabled: isShadowsEnabled)
-                .onChange(of: allAnchors, initial: true) { _, newAnchors in
-                    let mediaList = newAnchors.map { $0.mediaObject }
-                    
-                    // Update entities in ar view
-                    arView.updateAnchors(mediaList: mediaList)
-                }
-            } else {
+            // Can't run multiple reality views
+            if !isPinnedViewRendering || document.isGeneratingProjectThumbnail {
                 Color.clear
+            }
+            
+            else if _isCameraEnabled {
+                switch document.cameraFeedManager {
+                case .loaded(let cameraFeedManager):
+                    if let cameraFeedManager = cameraFeedManager.cameraFeedManager,
+                       let arView = cameraFeedManager.arView {
+                        CameraRealityView(arView: arView,
+                                          size: layerSize,
+                                          scale: scale,
+                                          opacity: opacity,
+                                          isShadowsEnabled: isShadowsEnabled)
+                        .onAppear {
+                            // Update list of node Ids using camera
+                            graph.enabledCameraNodeIds.insert(node.id)
+                        }
+                        .onChange(of: allAnchors, initial: true) { _, newAnchors in
+                            let mediaList = newAnchors.map { $0.mediaObject }
+                            
+                            // Update entities in ar view
+                            arView.updateAnchors(mediaList: mediaList)
+                        }
+                    }
+
+                case .loading, .failed:
+                    EmptyView()
+
+                case .none:
+                    Color.clear
+#if !targetEnvironment(macCatalyst)
+                        .onAppear {
+                            let nodeId = self.layerViewModel.id.layerNodeId.id
+                            document.realityViewCreatedWithoutCamera(graph: graph,
+                                                                     nodeId: nodeId)
+                        }
+#endif
+                    
+                }
+            }
+            
+            else {
+                NonCameraRealityView(size: layerSize,
+                                     scale: scale,
+                                     opacity: opacity,
+                                     isShadowsEnabled: isShadowsEnabled,
+                                     anchors: allAnchors)
             }
         }
         .modifier(PreviewCommonModifier(
--- Stitch/Graph/Node/Port/Model/PortValue/Type/Media/ObjectTypes/AR/RealityView.swift
+++ Stitch/Graph/Node/Port/Model/PortValue/Type/Media/ObjectTypes/AR/RealityView.swift
@@ -10,28 +10,54 @@ import RealityKit
 import SwiftUI
 import StitchSchemaKit
 
-struct RealityView: UIViewRepresentable {
+struct CameraRealityView: UIViewRepresentable {
     // AR view must already be created in media manager
     let arView: StitchARView
     let size: LayerSize
     let scale: Double
     let opacity: Double
-    let isCameraEnabled: Bool
     let isShadowsEnabled: Bool
-
+    
     func makeUIView(context: Context) -> StitchARView {
-        let view = arView
         arView.environment.background = .cameraFeed()
-        arView.cameraMode = isCameraEnabled ? .ar : .nonAR
+        arView.cameraMode = .ar
         arView.renderOptions = isShadowsEnabled ? [] : [.disableGroundingShadows]
-        return view
+        return arView
     }
 
     func updateUIView(_ uiView: StitchARView, context: Context) {
-        uiView.cameraMode = isCameraEnabled ? .ar : .nonAR
         uiView.frame.size = size.asAlgebraicCGSize
         uiView.alpha = opacity
         uiView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        uiView.renderOptions = isShadowsEnabled ? [] : [.disableGroundingShadows]
+    }
+}
+
+struct NonCameraRealityView: UIViewRepresentable {
+    let size: LayerSize
+    let scale: Double
+    let opacity: Double
+    let isShadowsEnabled: Bool
+    let anchors: [GraphMediaValue]
+    
+    func makeUIView(context: Context) -> StitchARView {
+        let arView = StitchARView(cameraMode: .nonAR)
+        arView.environment.background = .color(.clear)
+        arView.cameraMode = .nonAR
         arView.renderOptions = isShadowsEnabled ? [] : [.disableGroundingShadows]
+        return arView
+    }
+
+    func updateUIView(_ uiView: StitchARView, context: Context) {
+        // MARK: must update anchors in update view
+        let mediaList = anchors.map { $0.mediaObject }
+        
+        // Update entities in ar view
+        uiView.updateAnchors(mediaList: mediaList)
+        
+        uiView.frame.size = size.asAlgebraicCGSize
+        uiView.alpha = opacity
+        uiView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        uiView.renderOptions = isShadowsEnabled ? [] : [.disableGroundingShadows]
     }
 }
