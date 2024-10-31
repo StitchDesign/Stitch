04d65c8764318e04d22e5d46deed1f4f45aa4678
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-10-31 13:03:03 -0700

    cleanup

--- Stitch/Graph/Node/Layer/Type/Media/RealityView/PreviewRealityLayer.swift
+++ Stitch/Graph/Node/Layer/Type/Media/RealityView/PreviewRealityLayer.swift
@@ -19,7 +19,6 @@ struct PreviewRealityLayer: View {
     let parentDisablesPosition: Bool
     
     var body: some View {
-//        let nodeId = self.viewModel.id.layerNodeId.id
         let position = viewModel.position.getPosition ?? .zero
         let rotationX = viewModel.rotationX.asCGFloat
         let rotationY = viewModel.rotationY.asCGFloat
@@ -29,64 +28,50 @@ struct PreviewRealityLayer: View {
         let anchoring = viewModel.anchoring.getAnchoring ?? .defaultAnchoring
         let scale = viewModel.scale.asCGFloat
         
-//        switch document.cameraFeedManager {
-//        case .loaded(let cameraFeedManager):
-            if let node = document.visibleGraph.getNodeViewModel(viewModel.id.layerNodeId.asNodeId) {
-                @Bindable var node = node
-                
-                RealityLayerView(document: document,
-                                 graph: graph,
-                                 node: node,
-                                 layerViewModel: viewModel,
-                                 cameraFeedManager: document.cameraFeedManager?.loadedInstance?.cameraFeedManager,
-                                 isPinnedViewRendering: isPinnedViewRendering,
-                                 interactiveLayer: self.viewModel.interactiveLayer,
-                                 allAnchors: viewModel.allAnchors.compactMap { $0.asyncMedia },
-                                 position: position,
-                                 rotationX: rotationX,
-                                 rotationY: rotationY,
-                                 rotationZ: rotationZ,
-                                 size: size,
-                                 layerSize: layerSize,
-                                 scale: scale,
-                                 opacity: viewModel.opacity.asCGFloat,
-                                 anchoring: anchoring,
-                                 isCameraFeedEnabled: viewModel.isCameraEnabled.getBool ?? false,
-                                 isShadowsEnabled: viewModel.isShadowsEnabled.getBool ?? false,
-                                 blurRadius: viewModel.blurRadius.getNumber ?? .zero,
-                                 blendMode: viewModel.blendMode.getBlendMode ?? .defaultBlendMode,
-                                 brightness: viewModel.brightness.getNumber ?? .defaultBrightnessForLayerEffect,
-                                 colorInvert: viewModel.colorInvert.getBool ?? .defaultColorInvertForLayerEffect,
-                                 contrast: viewModel.contrast.getNumber ?? .defaultContrastForLayerEffect,
-                                 hueRotation: viewModel.hueRotation.getNumber ?? .defaultHueRotationForLayerEffect,
-                                 saturation: viewModel.saturation.getNumber ?? .defaultSaturationForLayerEffect,
-                                 pivot: viewModel.pivot.getAnchoring ?? .defaultPivot,
-                                 shadowColor: viewModel.shadowColor.getColor ?? .defaultShadowColor,
-                                 shadowOpacity: viewModel.shadowOpacity.getNumber ?? .defaultShadowOpacity,
-                                 shadowRadius: viewModel.shadowRadius.getNumber ?? .defaultShadowOpacity,
-                                 shadowOffset: viewModel.shadowOffset.getPosition ?? .defaultShadowOffset,
-                                 parentSize: parentSize,
-                                 parentDisablesPosition: parentDisablesPosition)
-            } else {
-                EmptyView()
-                    .onAppear {
+        if let node = document.visibleGraph.getNodeViewModel(viewModel.id.layerNodeId.asNodeId) {
+            @Bindable var node = node
+            
+            RealityLayerView(document: document,
+                             graph: graph,
+                             node: node,
+                             layerViewModel: viewModel,
+                             cameraFeedManager: document.cameraFeedManager?.loadedInstance?.cameraFeedManager,
+                             isPinnedViewRendering: isPinnedViewRendering,
+                             interactiveLayer: self.viewModel.interactiveLayer,
+                             allAnchors: viewModel.allAnchors.compactMap { $0.asyncMedia },
+                             position: position,
+                             rotationX: rotationX,
+                             rotationY: rotationY,
+                             rotationZ: rotationZ,
+                             size: size,
+                             layerSize: layerSize,
+                             scale: scale,
+                             opacity: viewModel.opacity.asCGFloat,
+                             anchoring: anchoring,
+                             isCameraFeedEnabled: viewModel.isCameraEnabled.getBool ?? false,
+                             isShadowsEnabled: viewModel.isShadowsEnabled.getBool ?? false,
+                             blurRadius: viewModel.blurRadius.getNumber ?? .zero,
+                             blendMode: viewModel.blendMode.getBlendMode ?? .defaultBlendMode,
+                             brightness: viewModel.brightness.getNumber ?? .defaultBrightnessForLayerEffect,
+                             colorInvert: viewModel.colorInvert.getBool ?? .defaultColorInvertForLayerEffect,
+                             contrast: viewModel.contrast.getNumber ?? .defaultContrastForLayerEffect,
+                             hueRotation: viewModel.hueRotation.getNumber ?? .defaultHueRotationForLayerEffect,
+                             saturation: viewModel.saturation.getNumber ?? .defaultSaturationForLayerEffect,
+                             pivot: viewModel.pivot.getAnchoring ?? .defaultPivot,
+                             shadowColor: viewModel.shadowColor.getColor ?? .defaultShadowColor,
+                             shadowOpacity: viewModel.shadowOpacity.getNumber ?? .defaultShadowOpacity,
+                             shadowRadius: viewModel.shadowRadius.getNumber ?? .defaultShadowOpacity,
+                             shadowOffset: viewModel.shadowOffset.getPosition ?? .defaultShadowOffset,
+                             parentSize: parentSize,
+                             parentDisablesPosition: parentDisablesPosition)
+        } else {
+            EmptyView()
+                .onAppear {
 #if DEBUG
-                        fatalError()
+                    fatalError()
 #endif
-                    }
-            }
-//            
-//        case .loading, .failed:
-//            EmptyView()
-//            
-//        case .none:
-//            // Note that EmptyView won't trigger the onApppear closure
-//            Color.clear
-//                .onAppear {
-//                    document.realityViewCreatedWithoutCamera(graph: graph,
-//                                                             nodeId: nodeId)
-//                }
-//        }
+                }
+        }
     }
 }
 
@@ -158,7 +143,6 @@ struct RealityLayerView: View {
                             // Update list of node Ids using camera
                             graph.enabledCameraNodeIds.insert(node.id)
                         }
-                        //                                      anchors: allAnchors)
                         .onChange(of: allAnchors, initial: true) { _, newAnchors in
                             let mediaList = newAnchors.map { $0.mediaObject }
                             
