f8f2848d4d8003e5566a3343b8c62ee64ce9614a
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-11-04 08:33:35 -0800

    Revert "Disabled number formatter for perf (#524)" (#538)

--- Stitch/Graph/Node/Port/Model/PortValue/Type/Layer/StitchSpacingUtils.swift
+++ Stitch/Graph/Node/Port/Model/PortValue/Type/Layer/StitchSpacingUtils.swift
@@ -57,8 +57,7 @@ extension StitchSpacing {
     var display: String {
         switch self {
         case .number(let x):
-            // return GlobalFormatter.string(for: x) ?? x.description
-            return x.description
+            return GlobalFormatter.string(for: x) ?? x.description
         case .between:
             return "Between"
         case .evenly:
