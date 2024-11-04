16980f7a53265ac82925168c7181aa2d84e7215f
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-11-04 10:31:20 -0800

    Fixed pulse reversion effects regression (#539)

--- Stitch/Graph/Node/Port/Model/PortValue/Type/Pulse/PulseReversionUtils.swift
+++ Stitch/Graph/Node/Port/Model/PortValue/Type/Pulse/PulseReversionUtils.swift
@@ -8,39 +8,22 @@
 import Foundation
 import StitchSchemaKit
 
-extension PortValuesList {
-    func getPulseReversionEffects(nodeId: NodeId,
-                                  graphTime: TimeInterval) -> SideEffects {
-        self.enumerated()
-            .flatMap { portId, values in
-                values.getPulseReversionEffects(nodeId: nodeId,
-                                                portId: portId,
-                                                graphTime: graphTime)
-            }
-    }
-}
-
 extension PortValues {
-    func getPulseReversionEffects(nodeId: NodeId,
-                                  portId: Int,
+    func getPulseReversionEffects(id: NodeIOCoordinate,
                                   graphTime: TimeInterval) -> SideEffects {
         self.flatMap {
-            $0.getPulseReversionEffects(nodeId: nodeId,
-                                        portId: portId,
+            $0.getPulseReversionEffects(id: id,
                                         graphTime: graphTime)
         }
     }
 }
 
 extension PortValue {
-    func getPulseReversionEffects(nodeId: NodeId,
-                                  portId: Int,
+    func getPulseReversionEffects(id: NodeIOCoordinate,
                                   graphTime: TimeInterval) -> SideEffects {
         if let lastTimePulsed = self.getPulse,
            lastTimePulsed.shouldPulse(graphTime) {
-            let pulsedIndex = NodeIOCoordinate(portId: portId,
-                                               nodeId: nodeId)
-            return getPostPulseEffects(.output(pulsedIndex))
+            return getPostPulseEffects(.output(id))
         }
         
         return []
