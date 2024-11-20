8e29b7bb410f06fa5b0bde2b167160cd4a79d7ca
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-11-20 12:39:23 -0800

    Fixed wireless receiver outgoing edge regression (#591)

--- Stitch/Graph/Node/Title/View/CanvasItemTitleView.swift
+++ Stitch/Graph/Node/Title/View/CanvasItemTitleView.swift
@@ -43,73 +43,13 @@ struct CanvasItemTitleView: View {
     var body: some View {
 //        logInView("NodeTitleView body \(id)")
         
-        // A Wireless Receiver node's title is not directly editable
-        if node.patch == .wirelessReceiver {
-            // logInView("NodeTitleView body isWirelessReceiver \(id)")
-            let _title = node.currentBroadcastChoiceId.flatMap { graph.getNodeViewModel($0)?.displayTitle } ?? name
-                        
-            Menu {
-                let choice = node.currentBroadcastChoice
-                NodeWirelessBroadcastSubmenuView(graph: graph,
-                                                 currentBroadcastChoice: choice ?? nilBroadcastChoice,
-//                                                 assignedBroadcaster: choice,
-                                                 nodeId: nodeId,
-                                                 forNodeTitle: true)
-            } label: {
-                StitchTextView(string: _title)
-            }
-            .buttonStyle(.plain)
-            .foregroundColor(STITCH_TITLE_FONT_COLOR)
-            .menuIndicator(.hidden)
-            
-        } else {
-            editableTitle
-                .font(STITCH_FONT)
-                .foregroundColor(STITCH_TITLE_FONT_COLOR)
-                .lineLimit(1)
-        }
-    }
-    
-    // TODO: need to provide some kind of additional width on the sides so that text is not chopped off as we type
-    // One approach that might work IF CACHED is this: https://github.com/vpl-codesign/stitch/pull/2796/files
-    // But there's some weird jump that happens when we finally commit the change?
-    
-    @MainActor @ViewBuilder
-    var editableTitle: some View {
-        // logInView("NodeTitleView editableTitle \(id)")
-        
         let label = name
-          
-        if node.patch == .mathExpression {
-            
-            let mathExpression = node.patchNode?.mathExpression
-            
-            VStack(alignment: .leading) {
-                // Always shows node title
-                NodeTitleTextField(graph: graph,
-                                   id: canvasId,
-                                   label: label,
-                                   isCanvasItemSelected: isCanvasItemSelected)
-                
-                // Always needs some math expression;
-                // if none yet exists (because math-expr node just created),
-                // use blank string.
-                .modifier(MathExpressionPopoverViewModifier(
-                    id: nodeId,
-                    mathExpression: mathExpression ?? "",
-                    isFocused: graph.graphUI.reduxFocusedField == .mathExpression(nodeId)))
-                
-                // Show formula if not empty
-                if let mathExpression = mathExpression,
-                   !mathExpression.isEmpty {
-                    StitchTextView(string: mathExpression,
-                                   fontColor: Color(.nodeTitleFont),
-                                   lineLimit: 1)
-                }
-            }
-        } else {
-            VStack(alignment: .leading) {
-                
+        let mathExpression = node.patchNode?.mathExpression
+
+        VStack(alignment: .leading) {
+            if node.patch == .wirelessReceiver {
+                wirelessReceiverMenuView
+            } else {
                 HStack {
                     if node.kind == .group {
                         Image(systemName: "folder")
@@ -121,13 +61,59 @@ struct CanvasItemTitleView: View {
                                        isCanvasItemSelected: isCanvasItemSelected)
                 }
                 
-                if let defaultTitle = node.getValidCustomTitle() {
-                    StitchTextView(string: defaultTitle,
+                // Always needs some math expression;
+                // if none yet exists (because math-expr node just created),
+                // use blank string.
+                .modifier(
+                    MathExpressionPopoverViewModifier(
+                        id: nodeId,
+                        shouldDisplay: node.patch == .mathExpression,
+                        mathExpression: mathExpression ?? "",
+                        isFocused: graph.graphUI.reduxFocusedField == .mathExpression(nodeId))
+                )
+                
+                // Show formula if not empty
+                if let customTitle = self.customTitleString {
+                    StitchTextView(string: customTitle,
                                    fontColor: Color(.nodeTitleFont),
                                    lineLimit: 1)
                 }
             }
         }
+    }
+    
+    // TODO: need to provide some kind of additional width on the sides so that text is not chopped off as we type
+    // One approach that might work IF CACHED is this: https://github.com/vpl-codesign/stitch/pull/2796/files
+    // But there's some weird jump that happens when we finally commit the change?
+    
+    var customTitleString: String? {
+        // Show formula if not empty
+        if let mathExpression = node.patchNode?.mathExpression,
+           !mathExpression.isEmpty {
+            return mathExpression
+        }
+        
+        return node.getValidCustomTitle()
+    }
+    
+    @ViewBuilder
+    var wirelessReceiverMenuView: some View {
+        let _title = node.currentBroadcastChoiceId.flatMap { graph.getNodeViewModel($0)?.displayTitle } ?? name
+                    
+        Menu {
+            let choice = node.currentBroadcastChoice
+            NodeWirelessBroadcastSubmenuView(graph: graph,
+                                             currentBroadcastChoice: choice ?? nilBroadcastChoice,
+//                                                 assignedBroadcaster: choice,
+                                             nodeId: nodeId,
+                                             forNodeTitle: true)
+        } label: {
+            StitchTextView(string: _title)
+                .height(NODE_TITLE_HEIGHT)
+        }
+        .buttonStyle(.plain)
+        .foregroundColor(STITCH_TITLE_FONT_COLOR)
+        .menuIndicator(.hidden)
     }
 }
 
