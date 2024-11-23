8cb11e94ec1ac5709c2cada66d3121cb804fd62a
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-11-22 19:47:26 -0800

    wip

--- Stitch/App/Util/TypeExtension/UIImageExtensionUtils.swift
+++ Stitch/App/Util/TypeExtension/UIImageExtensionUtils.swift
@@ -55,13 +55,18 @@ extension UIImage {
         if let output = currentFilter.outputImage,
            let cgImage = context.createCGImage(output, from: output.extent) {
             let uiImage = UIImage(cgImage: cgImage, scale: scale, orientation: imageOrientation)
-            uiImage.accessibilityIdentifier = self.accessibilityIdentifier
+            
+            await MainActor.run { [weak uiImage, weak self] in
+                guard let newIdentifier = self?.accessibilityIdentifier else { return }
+                uiImage?.accessibilityIdentifier = newIdentifier
+            }
             return .success(uiImage)
         }
         return .failure(.imageGrayscaleFailed)
     }
 
     /// Creates a deep copy of a UIImage reference.
+    @MainActor
     func clone() -> UIImage? {
         guard let originalCgImage = self.cgImage, let newCgImage = originalCgImage.copy() else {
             return nil
