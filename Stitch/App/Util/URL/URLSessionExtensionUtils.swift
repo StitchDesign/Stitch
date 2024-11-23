520c031760379f05e40affa1320015054d536645
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-11-23 13:43:57 -1000

    cleanup

--- Stitch/App/Util/URL/URLSessionExtensionUtils.swift
+++ Stitch/App/Util/URL/URLSessionExtensionUtils.swift
@@ -9,23 +9,6 @@ import Foundation
 import StitchSchemaKit
 import SwiftUI
 
-//
-//extension URLSession {
-//    public typealias FutureSessionDataTask = (URLSessionDataTask, Future<(Data?, URLResponse?), AnyError>)
-//    public typealias FutureSessionUploadTask = (URLSessionUploadTask, Future<(Data?, URLResponse?), AnyError>)
-//    public typealias FutureSessionDownloadTask = (URLSessionDownloadTask, Future<(URL?, URLResponse?), AnyError>)
-//
-//    public func dataTask(with request: URLRequest) -> FutureSessionDataTask {
-//        let p = Promise<(Data?, URLResponse?), AnyError>()
-//
-//        let task = self.dataTask(with: request, completionHandler: self.completionHandler(promise: p))
-//
-//        return (task, p.future)
-//    }
-//}
-
-// https://github.com/Thomvis/FutureProofing/blob/2d5c240dac1c71a6e4bca71834cf8448a6ea2e73/FutureProofing/BrightFuture%2BFutureProofing.swift
-
 // MARK: AnyError to allow materialize generique error
 public struct AnyError: Error {
     public let cause: Error
