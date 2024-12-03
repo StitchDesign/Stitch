fb6373de564fbeda17b497ef5b4c6275152143e0
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-12-03 15:25:35 -0800

    toggle camera support

--- Stitch/Graph/Node/Layer/Type/Media/RealityView/PreviewRealityLayer.swift
+++ Stitch/Graph/Node/Layer/Type/Media/RealityView/PreviewRealityLayer.swift
@@ -115,12 +115,26 @@ struct RealityLayerView: View {
     let parentDisablesPosition: Bool
     
     // Override camera setting on Mac
-    var _isCameraEnabled: Bool {
-#if targetEnvironment(macCatalyst)
-        return false
-#else
-        return isCameraFeedEnabled
+//    var _isCameraEnabled: Bool {
+//#if targetEnvironment(macCatalyst)
+//        return false
+//#else
+//        return isCameraFeedEnabled
+//#endif
+//    }
+    
+    @MainActor
+    func updateRealityContent(_ content: inout RealityViewCameraContent) {
+        // .spatialTracking not support on mac
+#if !targetEnvironment(macCatalyst)
+        content.camera = isCameraFeedEnabled ? .spatialTracking : .virtual
 #endif
+        
+        let content = content
+        
+        Task { @MainActor in
+            self.localRealityContent = content
+        }
     }
     
     var body: some View {
@@ -133,15 +147,9 @@ struct RealityLayerView: View {
             else {
                 ZStack {
                     RealityView { content in
-                        await MainActor.run {
-                            self.localRealityContent = content
-                        }
+                        self.updateRealityContent(&content)
                     } update: { content in
-                        let content = content
-                        
-                        Task { @MainActor in
-                            self.localRealityContent = content
-                        }
+                        self.updateRealityContent(&content)
                     }
 
                     GroupLayerNode.content(document: document,
