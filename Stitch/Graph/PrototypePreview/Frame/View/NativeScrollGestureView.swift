5ce346e5910cb38728cc4240f78c51f63c6a0327
Chris Clampitt <christian.clampitt@nyu.edu>
2024-12-04 14:38:03 -0700

    Clean up

--- Stitch/Graph/PrototypePreview/Frame/View/NativeScrollGestureView.swift
+++ Stitch/Graph/PrototypePreview/Frame/View/NativeScrollGestureView.swift
@@ -39,18 +39,17 @@ struct NativeScrollGestureView: ViewModifier {
 struct NativeScrollGestureViewInner: ViewModifier {
     
     let layerViewModel: LayerViewModel
-
-    // alternatively, pass in the
-    
     @Bindable var graph: GraphState
-                    
-    @State var scrollPosition: ScrollPosition = .init(edge: .top)
-    
+        
     // Raw, unchanged offset reported by the ScrollView; used to:
     // (1) offset the ScrollView's displacement of the layer and
     // (2) update the scroll interaction node's output
     @State var scrollOffset: CGPoint = .zero
-        
+    
+    // Programmatic manipulation of the ScrollView's position
+    @State var scrollPosition: ScrollPosition = .init(edge: .top)
+    
+    
     var nativeScrollState: NativeScrollInteractionLayer {
         layerViewModel.interactiveLayer.nativeScrollState
     }
@@ -58,129 +57,111 @@ struct NativeScrollGestureViewInner: ViewModifier {
     var scrollAxes: Axis.Set {
         self.nativeScrollState.scrollAxes
     }
-        
+    
     // Needed to force a complete re-render when
     // IDEALLY: keep offset
     @State var viewId: UUID = .init()
-        
+    
     func body(content: Content) -> some View {
+
+        ScrollView(scrollAxes) {
+            
+            content
+            
+            // apply additional `.frame` for custom content size; but only if that dimension is > 0
+                .frame(width: nativeScrollState.contentSize.width > 0 ? nativeScrollState.contentSize.width : nil)
+                .frame(height: nativeScrollState.contentSize.height > 0 ? nativeScrollState.contentSize.height : nil)
+            
+            // factor out parent-scroll's offset, so that view does not move unless we explicitly connect scroll interaction node's output to the layer's position input
+                .offset(x: self.scrollOffset.x,
+                        y: self.scrollOffset.y)
+        }
+        // Required for forced re-render when e.g. scrollable-axes change
+        .id(self.viewId)
         
-        logInView("NativeScrollGestureViewInner: body")
+        // TODO: allow user to control? requires forced re-rendering the view
+        .scrollIndicators(.hidden)
+        
+        // TODO: allow user to control?
+        .scrollBounceBehavior(.basedOnSize)
+        
+        // For programmatically manipulating the scroll view's position
+        .scrollPosition(self.$scrollPosition)
+        
+        .onScrollGeometryChange(for: CGPoint.self) { geometry in
+            // Note: the scroll view's reported value; can be manipulated, but does not affect the scroll view's scrolling, which has already happened
+            geometry.contentOffset
+        } action: { oldValue, newValue in
+            //                log("NativeScrollGestureViewInner: onScrollGeometryChange: newValue \(newValue) for layerViewModel.id \(layerViewModel.id)")
+            
+            // Always update the raw, unmodified scrollOfset, so that child is not automatically moved as parent moves
+            self.scrollOffset = newValue
+            
+            self.layerViewModel.interactiveLayer.nativeScrollState.rawScrollViewOffset = .init(
+                // why do you have to make these negative ? matches Origami
+                x: (-newValue.x).asPositiveZero,
+                y: (-newValue.y).asPositiveZero
+            )
+        } // .onScrollGeometryChange
+        
+        // TODO: how to tackle some of the awkward scrolling that happens after we toggle x/y scroll enabled ?
+        .onChange(of: self.scrollAxes, { oldValue, newValue in
+            log("NativeScrollGestureViewInner: scrollAxes changed")
+            self.viewId = .init()
+            log("NativeScrollGestureViewInner: scrollAxes changed: changed viewId")
+            self.scrollPosition.scrollTo(
+                x: self.scrollOffset.x,
+                y: self.scrollOffset.y)
+        })
+        
+        // Responding to changes to JumpStyle input
+        
+        .onChange(of: nativeScrollState.jumpToX) { _, newValue in
 
-            // Does view properly re-render if this change?
-            // Or do we need `.onChange(of: layerViewModel.scrollYEnabled) { self.id = .init() }` ?
-            ScrollView(scrollAxes) {
-                content
-
-                // apply additional `.frame` for custom content size; but only if that dimension is > 0
-                    .frame(width: nativeScrollState.contentSize.width > 0 ? nativeScrollState.contentSize.width : nil)
-                
-                    .frame(height: nativeScrollState.contentSize.height > 0 ? nativeScrollState.contentSize.height : nil)
-                                
-                    // factor out parent-scroll's offset
-                    .offset(x: self.scrollOffset.x,
-                            y: self.scrollOffset.y)
+            guard newValue && nativeScrollState.xScrollEnabled else {
+                return
             }
-            // Required for forced re-render when e.g. scrollable-axes change
-            .id(self.viewId)
             
-            // TODO: allow user to control? requires forced re-rendering the view
-            .scrollIndicators(.hidden)
+            let jump = {
+                self.scrollPosition.scrollTo(x: nativeScrollState.jumpPositionX,
+                                             // Must specify `y:` as well, so that y is not set to 0
+                                             y: self.scrollOffset.y)
+            }
             
-            // TODO: allow user to control?
-            .scrollBounceBehavior(.basedOnSize)
-
-            // For programmatically manipulating the scroll view's position
-            .scrollPosition(self.$scrollPosition)
-            
-            .onScrollGeometryChange(for: CGPoint.self) { geometry in
-                // Note: the scroll view's reported value; can be manipulated, but does not affect the scroll view's scrolling, which has already happened
-                geometry.contentOffset
-            } action: { oldValue, newValue in
-//                log("NativeScrollGestureViewInner: onScrollGeometryChange: newValue \(newValue) for layerViewModel.id \(layerViewModel.id)")
-                
-                // Always update the raw, unmodified scrollOfset, so that child is not automatically moved as parent moves
-                self.scrollOffset = newValue
-                
-                self.layerViewModel.interactiveLayer.nativeScrollState.rawScrollViewOffset = .init(
-                        // why do you have to make these negative ? matches Origami
-                        x: (-newValue.x).asPositiveZero,
-                        y: (-newValue.y).asPositiveZero
-                    )
-            } // .onScrollGeometryChange
-            
-            // TODO: how to tackle some of the awkward scrolling that happens after we toggle x/y scroll enabled ?
-            .onChange(of: self.scrollAxes, { oldValue, newValue in
-                log("NativeScrollGestureViewInner: scrollAxes changed")
-                self.viewId = .init()
-                log("NativeScrollGestureViewInner: scrollAxes changed: changed viewId")
-                self.scrollPosition.scrollTo(
-                    x: self.scrollOffset.x,
-                    y: self.scrollOffset.y)
-            })
-            
-            // Responding to changes to JumpStyle input
-            
-            .onChange(of: layerViewModel.interactiveLayer.nativeScrollState.jumpToX) { _, newValue in
-                log(".onChange(of: nativeScrollState.jumpToX)")
-                
-                guard newValue && nativeScrollState.xScrollEnabled else {
-                    return
-                }
-                
-                let jump = {
-                    self.scrollPosition.scrollTo(x: nativeScrollState.jumpPositionX,
-                                                 // Must specify `y:` as well, so that y is not set to 0
-                                                 y: self.scrollOffset.y)
-                }
-                
-                if nativeScrollState.jumpStyleX == .animated {
-                    withAnimation {
-                        jump()
-                    }
-                } else {
+            if nativeScrollState.jumpStyleX == .animated {
+                withAnimation {
                     jump()
                 }
-            } // .onChange
+            } else {
+                jump()
+            }
+        } // .onChange
+        
+        .onChange(of: nativeScrollState.jumpToY) { _, newValue in
             
-            .onChange(of: layerViewModel.interactiveLayer.nativeScrollState.jumpToY) { _, newValue in
-                log(".onChange(of: nativeScrollState.jumpToY)")
-                
-                guard newValue && nativeScrollState.yScrollEnabled else {
-                    return
-                }
-                
-                let jump = {
-                    self.scrollPosition.scrollTo(x: self.scrollOffset.x,
-                                                 y: nativeScrollState.jumpPositionY)
-                }
-                
-                if nativeScrollState.jumpStyleY == .animated {
-                    withAnimation {
-                        jump()
-                    }
-                } else {
+            guard newValue && nativeScrollState.yScrollEnabled else {
+                return
+            }
+            
+            let jump = {
+                self.scrollPosition.scrollTo(x: self.scrollOffset.x,
+                                             y: nativeScrollState.jumpPositionY)
+            }
+            
+            if nativeScrollState.jumpStyleY == .animated {
+                withAnimation {
                     jump()
                 }
-            } // .onChange
-
-        
-            .onChange(of: layerViewModel.interactiveLayer.nativeScrollState.graphReset) { _, newValue in
-                log(".onChange(of: nativeScrollState.graphReset)")
-                if newValue {
-                    self.scrollPosition.scrollTo(edge: .top)
-                }
-            } // .onChange
+            } else {
+                jump()
+            }
+        } // .onChange
         
         
-        // This is constantly triggering a re-render of the view,
-        // which makes us listen for interactiveLayer changes
-        
-//            .onChange(of: graph.graphStepState.graphFrameCount) { _, newValue in
-//                if newValue == Int(2) {
-//                    self.scrollPosition.scrollTo(edge: .top)
-//                }
-//            } // .onChange
-                            
+        .onChange(of: layerViewModel.interactiveLayer.nativeScrollState.graphReset) { _, newValue in
+            if newValue {
+                self.scrollPosition.scrollTo(edge: .top) // top-left corner
+            }
+        } // .onChange
     } // var body
 }
