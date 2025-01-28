f1d01962bf0ecc306e80a76ed3a20adf1e585556
Chris Clampitt <christian.clampitt@nyu.edu>
2025-01-28 10:17:07 -0800

    Do not open LLM Edit Modal when regular recording session ends (#724)
    * Do not open LLM Edit Modal when regular recording session ends

--- Stitch/Graph/StitchAI/GraphPrompting/UI/StitchAIPromptView.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/UI/StitchAIPromptView.swift
@@ -7,51 +7,8 @@
 
 import SwiftUI
 
-struct StitchAIPromptEntryModalView: View {
-    @Binding var prompt: String
-    let isGenerating: Bool
-    let onSubmit: (String) -> Void
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            StitchTextView(string: "Enter prompt to generate a project")
-            Divider()
-            TextEditor(text: $prompt)
-                .font(STITCH_FONT)
-                .scrollContentBackground(.hidden)
-            
-            if isGenerating {
-                Text("Generating response...")
-                    .font(STITCH_FONT)
-                    .foregroundColor(.gray)
-                    .padding(.top)
-            } else {
-                Button("Submit") {
-                    onSubmit(prompt)
-                }
-                .disabled(prompt.isEmpty)
-                .padding(.top)
-            }
-        }
-        .padding()
-    }
-}
-
 struct StitchAIState: Equatable {
-    var promptState = StitchAIPromptState()
-}
-
-struct StitchAIPromptState: Equatable {
-    var showModal = false
-    var prompt: String = ""
-    var isGenerating = false
+    // Saved prompt from insert-node-menu, used during retries
+    // TODO: don't need this? can just grab from InsertNodeMenuState.searchQuery?
     var lastPrompt: String? = nil
-}
-
-struct StitchAIPromptEdited: StitchDocumentEvent {
-    let prompt: String
-    
-    func handle(state: StitchDocumentViewModel) {
-        state.stitchAI.promptState.prompt = prompt
-    }
 }
