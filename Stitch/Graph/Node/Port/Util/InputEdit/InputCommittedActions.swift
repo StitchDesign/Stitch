4737848ae20cf61c77c640bc3f9c43064871eda5
Chris Clampitt <christian.clampitt@nyu.edu>
2025-01-29 20:14:51 -0800

    Add method to Anchoring; pass NodeId to inputEditCommited

--- Stitch/Graph/Node/Port/Util/InputEdit/InputCommittedActions.swift
+++ Stitch/Graph/Node/Port/Util/InputEdit/InputCommittedActions.swift
@@ -23,6 +23,7 @@ extension GraphState {
         }
         
         self.inputEditCommitted(input: input,
+                                nodeId: node.id,
                                 value: value,
                                 wasDropdown: true, // true?
                                 wasAdjustmentBarSelection: false)
@@ -62,6 +63,7 @@ extension GraphState {
             // Note: heterogenous values doesn't matter; only the multiselect does
             layerMultiselectInput.multiselectObservers(self).forEach { observer in
                 self.inputEditCommitted(input: observer.rowObserver,
+                                        nodeId: node.id,
                                         value: value,
                                         wasDropdown: wasDropdown,
                                         wasAdjustmentBarSelection: wasAdjustmentBarSelection)
@@ -71,6 +73,7 @@ extension GraphState {
         // just editing a single
         else {
             self.inputEditCommitted(input: input,
+                                    nodeId: node.id,
                                     value: value,
                                     wasDropdown: wasDropdown,
                                     wasAdjustmentBarSelection: wasAdjustmentBarSelection)
@@ -79,11 +82,13 @@ extension GraphState {
     
     @MainActor
     func inputEditCommitted(input: InputNodeRowObserver,
+                            nodeId: NodeId,
                             value: PortValue?,
                             wasDropdown: Bool,
                             wasAdjustmentBarSelection: Bool = false) {
         
-        guard let nodeId = input.nodeDelegate?.id,
+        // TODO: debug: why was input.nodeDelegate `nil` for e.g. the padding layer-input but not the size layer-input, and only in the context of generating an LLM action?
+        guard // let nodeId = input.nodeDelegate?.id,
               let nodeViewModel = self.getNodeViewModel(nodeId),
               var value = value else {
             log("GraphState.inputEditCommitted error: could not find node data.")
