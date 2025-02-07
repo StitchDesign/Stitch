df68a3b36645213b1621268c84b05ea57b3d5dbb
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-02-06 22:31:51 -0800

    fix tests

--- StitchTests/openAIRequestTests.swift
+++ StitchTests/openAIRequestTests.swift
@@ -9,20 +9,8 @@ import XCTest
 @testable import Stitch
 
 class OpenAIRequestTests: XCTestCase {
-    
-    func testSecretsVariablesNotNil() {
-        #if STITCH_AI
-        XCTAssertNotNil(Secrets.openAIAPIKey)
-        XCTAssertFalse(Secrets.openAIAPIKey.isEmpty)
-        
-        XCTAssertNotNil(Secrets.openAIModel)
-        XCTAssertFalse(Secrets.openAIModel.isEmpty)
-        #else
-        XCTAssertNil(Secrets.openAIAPIKey)
-        XCTAssertTrue(Secrets.openAIAPIKey.isEmpty)
-        
-        XCTAssertNil(Secrets.openAIModel)
-        XCTAssertTrue(Secrets.openAIModel.isEmpty)
-        #endif
+    func testSecretsNotNil() {
+        let secrets = try? Secrets()
+        XCTAssertNotNil(secrets)
     }
 }
