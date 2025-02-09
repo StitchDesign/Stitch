e18acfd423c33e16a7d6567457bda9dbfed26eac
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-02-08 17:34:55 -0800

    Fixes Stitch AI to support release builds (#775)

--- Stitch/App/FeatureFlags.swift
+++ Stitch/App/FeatureFlags.swift
@@ -13,7 +13,7 @@ struct FeatureFlags {
     static let USE_COMMENT_BOX_FLAG: Bool = false
     
     #if STITCH_AI
-    static let USE_COMPONENTS = true
+    static let USE_COMPONENTS = false
     static let USE_AI_MODE = true
     #else
     static let USE_COMPONENTS = false
