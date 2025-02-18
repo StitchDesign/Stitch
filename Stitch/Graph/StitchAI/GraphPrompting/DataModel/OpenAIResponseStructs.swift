4a8af2d9cd258138a6cb78ef5f30d828b1074e2c
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-02-17 16:09:06 -0800

    Improved Stitch AI connection creation accuracy

--- Stitch/Graph/StitchAI/GraphPrompting/DataModel/OpenAIResponseStructs.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/DataModel/OpenAIResponseStructs.swift
@@ -123,24 +123,37 @@ struct ContentJSON: Codable {
 
 extension ContentJSON {
     static func exampleData() -> Self {
-        let id = UUID()
-        let addNode = StepActionAddNode(nodeId: id, nodeName: .patch(.add))
-        let setInput1 = StepActionSetInput(nodeId: id,
+        let id0 = UUID()
+        let id1 = UUID()
+        
+        let addNode = StepActionAddNode(nodeId: id0, nodeName: .patch(.add))
+        let textNode = StepActionAddNode(nodeId: id1, nodeName: .layer(.text))
+        
+        let setInput1 = StepActionSetInput(nodeId: id0,
                                           port: .portIndex(0),
                                           value: .number(3),
                                           valueType: .number)
-        let setInput2 = StepActionSetInput(nodeId: id,
+        let setInput2 = StepActionSetInput(nodeId: id0,
                                           port: .portIndex(1),
                                           value: .number(5),
                                           valueType: .number)
-        let changeType = StepActionChangeValueType(nodeId: id,
+        let changeType = StepActionChangeValueType(nodeId: id0,
                                                   valueType: .string)
+        
+        let makeConnection = StepActionConnectionAdded(
+            port: .keyPath(.init(layerInput: .text,
+                                 portType: .packed)),
+            toNodeId: id1,
+            fromPort: 0,
+            fromNodeId: id0)
         
         let steps: [Step] = [
             addNode.toStep,
+            textNode.toStep,
             setInput1.toStep,
             setInput2.toStep,
-            changeType.toStep
+            changeType.toStep,
+            makeConnection.toStep
         ]
         
         return .init(steps: steps)
