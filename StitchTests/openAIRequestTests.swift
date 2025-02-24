80a080aff7c19288c91225d992def66224e425a1
Chris Clampitt <christian.clampitt@nyu.edu>
2025-02-24 14:56:48 -0700

    wip

--- StitchTests/openAIRequestTests.swift
+++ StitchTests/openAIRequestTests.swift
@@ -19,29 +19,45 @@ class OpenAIRequestTests: XCTestCase {
     
     /// Tests conversions to and from decoded state. StitchAI sometimes uses different types, this ensures types are compatible.
     func testStitchAICodables() {
-        for type in NodeType.allCases {
-            if type == .none || type == .anchorEntity {
-                print("testStitchAICodables: skipping type: \(type)")
-                continue
-            }
+        for type in NodeType.allCases.filter({ $0 != .none && $0 != .anchorEntity }) {
+//            if type == .none || type == .anchorEntity {
+//                print("testStitchAICodables: skipping type: \(type)")
+//                continue
+//            }
+            
+            print("testStitchAICodables: testing type: \(type)")
           
             let portValue = type.defaultPortValue
             let valueCodable = portValue.anyCodable
             let portValueType = type.portValueTypeForStitchAI
             
-            do {
-                let encoding = try getStitchEncoder().encode(valueCodable)
-                
-                let jsonString = String(data: encoding, encoding: .utf8)
-                print("Json test: \(jsonString)")
-                
-                let decoding = try getStitchDecoder()
-                    .decodeStitchAI(portValueType,
-                                    data: encoding)
-            } catch {
-                print("failed to encode type \(type)")
-                XCTFail(error.localizedDescription)
+            guard let encoding = try? getStitchEncoder().encode(valueCodable) else {
+                XCTFail("Could not encode type \(type)")
+                fatalError()
             }
+            
+            let jsonString = String(data: encoding, encoding: .utf8)
+            print("encoding as json: \(jsonString)")
+            
+            guard let decoder = try? getStitchDecoder().decodeStitchAI(portValueType, data: encoding) else {
+                XCTFail("Could not decode type \(type)")
+                fatalError()
+            }
+            
+//            do {
+//                let encoding = try getStitchEncoder().encode(valueCodable)
+//                
+//                let jsonString = String(data: encoding, encoding: .utf8)
+//                print("encoding as json: \(jsonString)")
+//                
+//               
+//                
+//                print("successfully decoded")
+//                
+//            } catch {
+//                print("failed to encode type \(type), with error: \(error)")
+////                XCTFail(error.localizedDescription)
+//            }
             
             
         }
