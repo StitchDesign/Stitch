0953ed7a8784c0a01ab6c21bb4e58ac2811b100d
Chris Clampitt <christianjosiahclampitt@gmail.com>
2025-04-14 17:10:56 -0600

    Fix TrimText crash and inaccuracy; leverage native Swift prefix method (#1069)

--- Stitch/App/Util/TypeExtension/StringUtils.swift
+++ Stitch/App/Util/TypeExtension/StringUtils.swift
@@ -11,25 +11,9 @@ import StitchSchemaKit
 
 // https://stackoverflow.com/a/39677704/7170123
 extension String {
-
-    func index(from: Int) -> Index {
-        return self.index(startIndex, offsetBy: from)
-    }
-
     func substring(from: Int) -> String {
-        let fromIndex = index(from: from)
+        let fromIndex = self.index(self.startIndex, offsetBy: from)
         return String(self[fromIndex...])
-    }
-
-    func substring(to: Int) -> String {
-        let toIndex = index(from: to)
-        return String(self[..<toIndex])
-    }
-
-    func substring(with r: Range<Int>) -> String {
-        let startIndex = index(from: r.lowerBound)
-        let endIndex = index(from: r.upperBound)
-        return String(self[startIndex..<endIndex])
     }
 }
 
