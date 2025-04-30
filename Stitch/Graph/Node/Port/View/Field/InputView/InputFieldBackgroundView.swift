46f6ff14d35635f08dcb61de8a3ee50577905d0f
Chris Clampitt <christian.clampitt@nyu.edu>
2025-04-29 19:32:38 -0600

    cleanup

--- Stitch/Graph/Node/Port/View/Field/InputView/InputFieldBackgroundView.swift
+++ Stitch/Graph/Node/Port/View/Field/InputView/InputFieldBackgroundView.swift
@@ -8,84 +8,6 @@
 import SwiftUI
 
 
-// TODO: per Elliot, this is actually a perf-expensive view?
-
-//// NO LONGER ACTUALLY USED ? OR, NEEDS TO BE REPURPOSED ?
-//struct InputFieldBackground: ViewModifier {
-//        
-//    let show: Bool // if hovering, selected or for sidebar
-//    let hasDropdown: Bool
-//    let forPropertySidebar: Bool
-//    let isSelectedInspectorRow: Bool
-//    let isCanvasField: Bool
-//    var width: CGFloat
-//    let isHovering: Bool
-//    
-//    let onTap: (() -> Void)?
-//     
-////    static let HOVER_EXTRA_LENGTH: CGFloat = 80
-////    static let HOVER_EXTRA_LENGTH: CGFloat = 100
-//    static let HOVER_EXTRA_LENGTH: CGFloat = 52
-//    
-//    var hoveringAdjustment: CGFloat {
-//        isHovering ? Self.HOVER_EXTRA_LENGTH : 0
-//    }
-//    
-//    var widthAdjustedForDropdown: CGFloat {
-//        width - (hasDropdown ? (COMMON_EDITING_DROPDOWN_CHEVRON_WIDTH + 2) : 0.0)
-//    }
-//    
-//    var backgroundColor: Color {
-//        if forPropertySidebar {
-//            return Color.INSPECTOR_FIELD_BACKGROUND_COLOR
-//        } else {
-//            return Color.COMMON_EDITING_VIEW_READ_ONLY_BACKGROUND_COLOR
-//        }
-//    }
-//    
-//    func body(content: Content) -> some View {
-//        content
-//            
-//        
-//        // When this field uses a dropdown,
-//        // we shrink the "typeable" area of the input,
-//        // so that typing never touches the dropdown's menu indicator.
-//            .frame(width: widthAdjustedForDropdown, alignment: .leading)
-//        
-//        // ... But we always use a full-width background for the focus/hover effect.
-//            .frame(width: width, alignment: .leading)
-//            .padding([.leading, .top, .bottom], 2)
-//        
-//            .modifier(InputFieldBackgroundColorView(show: show))
-//            
-//        // iPad only
-//        #if !targetEnvironment(macCatalyst)
-//            .modifier(InspectorSelectedRowFieldBackground(isSelectedInspectorRow: isSelectedInspectorRow))
-//        #endif
-//        
-//            .contentShape(Rectangle())
-//        
-//        // Canvas field ONLY
-////            .overlay(content: {
-////                if isHovering, isCanvasField {
-////                    content
-////                        .frame(width: width + hoveringAdjustment,
-////                               alignment: .leading)
-////                        .padding([.leading, .top, .bottom], 2)
-////                        .modifier(InputFieldBackgroundColorView(show: show))
-////                        
-////                        .offset(x: hoveringAdjustment / 2)
-////                        
-////                        .onTapGesture {
-////                            if let onTap = self.onTap {
-////                                onTap() // focuses but then de-focuses
-////                            }
-////                        }
-////                }
-////            })
-//    }
-//}
-
 struct InputFieldFrameAndPadding: ViewModifier {
     
     /*
@@ -96,14 +18,7 @@ struct InputFieldFrameAndPadding: ViewModifier {
     let width: CGFloat
     
     let hasPicker: Bool
-    
-    // Actually: inspector's
-//    var hasDropdown: Bool {
-//        hasChoices && (isForCanvas || isForFlyout)
-////        isForCanvas || isForFlyout
-////        isForCanvas
-//    }
-    
+        
     var widthAdjustedForDropdown: CGFloat {
         width - (hasPicker ? (COMMON_EDITING_DROPDOWN_CHEVRON_WIDTH + 2) : 0.0)
     }
