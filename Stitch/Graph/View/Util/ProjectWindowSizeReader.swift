3dac0ce41882fbaab4f0a7e3f421607ffcd4b1e8
Chris Clampitt <christianjosiahclampitt@gmail.com>
2025-05-05 16:08:52 -0600

    Resize preview window when Mac Catalyst window resizes (#1200)

--- Stitch/Graph/View/Util/ProjectWindowSizeReader.swift
+++ Stitch/Graph/View/Util/ProjectWindowSizeReader.swift
@@ -27,29 +27,25 @@ struct ProjectWindowSizeReader: View {
     let isFullScreen: Bool
     @Binding var showFullScreenAnimateCompleted: Bool
     let showFullScreenObserver: AnimatableBool
-
-//    @Binding var menuHeight: CGFloat
+    
     let menuHeight: CGFloat
-//    @Binding var screenSize: CGSize
-
+    
     @State private var defaultLandscapeSize: CGSize?
-
+    
     var body: some View {
         GeometryReader { geometry in
             Color.clear
                 .onAppear {
-
+                    
                     // log("SIZE READING: ProjectWindowSizeReader: onAppear: geometry.size: \(geometry.size)")
-
-//                    screenSize = geometry.size
-
+                    
                     if geometry.size.isLandscape {
                         self.defaultLandscapeSize = geometry.size
                     }
-
+                    
                     self.previewWindowSizing.previewWindowDeviceSize = previewWindowSize
                     self.previewWindowSizing.userDeviceSize = geometry.size
-
+                    
                     // Creates full screen animation effect for iPhone experience
                     if isFullScreen {
                         showFullScreenAnimateCompleted = false
@@ -58,28 +54,33 @@ struct ProjectWindowSizeReader: View {
                         }
                     }
                 }
-
-                // TODO: if we want to resize the prototype window when Mac app window or iOS device orientation changes, decide how to coorindate that with user's manual-drag changes of prototype window size.
-                // Listener here needed for repainting the view after a device orientation change
+            
+            // TODO: if we want to resize the prototype window when Mac app window or iOS device orientation changes, decide how to coorindate that with user's manual-drag changes of prototype window size.
+            // Listener here needed for repainting the view after a device orientation change
                 .onChange(of: geometry.size) { oldValue, newValue in
-
+                    
                     //// log("ProjectWindowSizeReader: onChange of: geometry.size: \(geometry.size)")
                     // log("SIZE READING: ProjectWindowSizeReader: onChange of: oldValue: \(oldValue)")
                     // log("SIZE READING: ProjectWindowSizeReader: onChange of: newValue: \(newValue)")
-
+                    
                     if isFullScreen {
                         // log("ProjectWindowSizeReader: onChange of: geometry.size: we are fullscreen, so will update previewWindowSizing")
                         self.previewWindowSizing.userDeviceSize = geometry.size
                     }
-
-                    // TODO: ignore geometry changes when magic keyboard attached, so that we don't "slightly move up" the insert node menu?
-//                    screenSize = geometry.size
-
+                    
+#if targetEnvironment(macCatalyst)
+                    
+                    // Update 'device size' which for Catalyst is the Mac App window
+                    self.previewWindowSizing.userDeviceSize = geometry.size
+                    
+#else
+                    // iPad takes into account switches between portrait vs landscape orientation
+                    
                     if !self.defaultLandscapeSize.isDefined,
                        geometry.size.isLandscape {
                         self.defaultLandscapeSize = geometry.size
                     }
-
+                    
                     // If we're in landscape mode,
                     // and we're less than our defaultSize.height,
                     // then we use the MINIMUM HEIGHT for the node menu,
@@ -87,58 +88,59 @@ struct ProjectWindowSizeReader: View {
                        let defaultLandscapeSize = self.defaultLandscapeSize,
                        // TODO: figure out how to ignore the small change from magic keyboard ?
                        geometry.size.height < defaultLandscapeSize.height {
-
+                        
                         // log("ProjectWindowSizeReader: onChange of: geometry.size: setting min height: geometry.size.height: \(geometry.size.height)")
-
+                        
                         // log("ProjectWindowSizeReader: onChange of: geometry.size: setting min height: defaultLandscapeSize.height: \(defaultLandscapeSize.height)")
-
+                        
                         let heightDiff = defaultLandscapeSize.height - geometry.size.height
-
+                        
                         // log("ProjectWindowSizeReader: onChange of: geometry.size: setting min height: heightDiff: \(heightDiff)")
-
+                        
                         /*
                          EXAMPLE:
-
+                         
                          Default landscape height on an 11” iPad Pro = 834
-
+                         
                          Height when Full screen KB = 332
                          So diff = 834 - 332 = 502
-
+                         
                          Height when non-FS KB = 740
                          So diff = 834 - 740 = 94
-
+                         
                          Height when Magic Keyboard attached = 691
                          So diff = 834 - 691 = 143
                          */
                         // TODO: test on iPad Mini etc.
                         // TODO: why were we getting 299 vs 300 vs ... ?
                         //                        if heightDiff.magnitude > 300 {
-                                                
+                        
                         if heightDiff.magnitude > 295 {
-                             // log("ProjectWindowSizeReader: onChange of: geometry.size: setting to min height")
+                            // log("ProjectWindowSizeReader: onChange of: geometry.size: setting to min height")
                             //menuHeight = INSERT_NODE_MENU_MIN_HEIGHT
                             dispatch(NodeMenuHeightSet(newHeight: INSERT_NODE_MENU_MIN_HEIGHT))
-
+                            
                             // TODO: only adjust screen size if geometry changed enough to change the menu's height? Be careful about e.g. maybe us wanting to move up the menu when in portrait mode.
                             // screenHeight = geometry.size.height
-
+                            
                         } else {
-                             // log("ProjectWindowSizeReader: onChange of: geometry.size: diff not big enough")
+                            // log("ProjectWindowSizeReader: onChange of: geometry.size: diff not big enough")
                             //menuHeight = INSERT_NODE_MENU_MAX_HEIGHT
                             dispatch(NodeMenuHeightSet(newHeight: INSERT_NODE_MENU_MAX_HEIGHT))
                         }
                     }
-
+                    
                     // Needed when e.g. keyboard dimissed?
                     // or we rotate the device, e.g. into portrait mode?
                     else {
-                         // log("ProjectWindowSizeReader: onChange of: geometry.size: setting to max height again")
-//                        menuHeight = INSERT_NODE_MENU_MAX_HEIGHT
+                        // log("ProjectWindowSizeReader: onChange of: geometry.size: setting to max height again")
+                        //                        menuHeight = INSERT_NODE_MENU_MAX_HEIGHT
                         dispatch(NodeMenuHeightSet(newHeight: INSERT_NODE_MENU_MAX_HEIGHT))
                     }
+#endif
                 }
             // TODO: why does this logic live here? we're changing some ephemeral parts of preview-window data (size etc.) in a reader that's mostly about user-device-screen changes (e.g. keyboard, rotation) ?
-                // i.e. when user changes preview window size via settings menu
+            // i.e. when user changes preview window size via settings menu
                 .onChange(of: previewWindowSize) { oldValue, newValue in
                     
                     self.previewWindowSizing.previewWindowDeviceSize = newValue
@@ -149,7 +151,7 @@ struct ProjectWindowSizeReader: View {
                 }
             
                 .onChange(of: isFullScreen) { oldValue, newValue in
-
+                    
                     showFullScreenAnimateCompleted = false
                     withAnimation(.stitchAnimation) {
                         if newValue {
@@ -160,15 +162,13 @@ struct ProjectWindowSizeReader: View {
                 }
         }
     }
-
-    @State var parentSizeBeforeFullscreen: CGSize = .zero
 }
 
 extension CGSize {
     var isLandscape: Bool {
         self.width > self.height
     }
-
+    
     var isPortrait: Bool {
         self.height > self.width
     }
