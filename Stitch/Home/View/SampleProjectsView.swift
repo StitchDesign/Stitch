adc9a3b8b442a2985579edd2ce104457e9196a3a
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-05-07 03:10:55 -0700

    removed grid

--- Stitch/Home/View/SampleProjectsView.swift
+++ Stitch/Home/View/SampleProjectsView.swift
@@ -31,36 +31,7 @@ extension SampleProjectData: Identifiable {
 }
 
 struct SampleProjectsView: View {
-    static let _dataList: [SampleProjectData?] = [
-        SampleProjectData(projectName: "Monthly Stays (Josh Pekera)",
-                          projectURL: "Monthly_Stays/Monthly%20Stays%20(Josh%20Pekera).stitch",
-                          projectAssetName: "MonthlyStays"),
-        SampleProjectData(projectName: "Music Player (GK3)",
-                          projectURL: "Music_Player/Music%20Player%20(GK3).stitch",
-                          projectAssetName: "MusicPlayer"),
-        SampleProjectData(projectName: "Hello World",
-                          projectURL: "Hello_World/Hello%20World.stitch",
-                          projectAssetName: "HelloWorld"),
-        SampleProjectData(projectName: "Wallet",
-                          projectURL: "Wallet/Wallet%20(Wayne%20Sang).stitch",
-                          projectAssetName: "Wallet"),
-        SampleProjectData(projectName: "AR Robot (Elliot)",
-                          projectURL:"AR_Robot/AR%20Robot%20(Elliot).stitch",
-                          projectAssetName: "ARRobot")
-    ]
-    
-    static let gridItems: [GridItem] = [
-        .init(.adaptive(minimum: PROJECTSVIEW_ITEM_WIDTH), spacing: 20),
-        .init(.adaptive(minimum: PROJECTSVIEW_ITEM_WIDTH), spacing: 20)
-    ]
-    
-    let dataList: [SampleProjectData]
     @Bindable var store: StitchStore
-    
-    init(store: StitchStore) {
-        self.dataList = Self._dataList.compactMap { $0 }
-        self.store = store
-    }
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
@@ -69,12 +40,63 @@ struct SampleProjectsView: View {
     }
     
     var sampleProjectsList: some View {
-        LazyVGrid(columns: Self.gridItems, alignment: .center) {
-            ForEach(self.dataList) { data in
+        VStack {
+            HStack {
                 SampleProjectView(store: store,
-                                  data: data)
+                                  data: .init(projectName: "Monthly Stays (Josh Pekera)",
+                                              projectURL: "Monthly_Stays/Monthly%20Stays%20(Josh%20Pekera).stitch",
+                                              projectAssetName: "MonthlyStays")
+                )
+                SampleProjectView(store: store,
+                                  data: .init(projectName: "Music Player (GK3)",
+                                              projectURL: "Music_Player/Music%20Player%20(GK3).stitch",
+                                              projectAssetName: "MusicPlayer")
+                )
+                SampleProjectView(store: store,
+                                  data: .init(projectName: "Hello World",
+                                              projectURL: "Hello_World/Hello%20World.stitch",
+                                              projectAssetName: "HelloWorld")
+                )
+            }
+            
+            HStack {
+                SampleProjectView(store: store,
+                                  data: .init(projectName: "Wallet",
+                                              projectURL: "Wallet/Wallet%20(Wayne%20Sang).stitch",
+                                              projectAssetName: "Wallet")
+                )
+                SampleProjectView(store: store,
+                                  data: .init(projectName: "AR Robot (Elliot)",
+                                              projectURL:"AR_Robot/AR%20Robot%20(Elliot).stitch",
+                                              projectAssetName: "ARRobot")
+                )
+                
+                newProjectButton
             }
         }
+    }
+    
+    var newProjectButton: some View {
+        Button {
+            Task(priority: .high) { [weak store] in
+                await store?.createNewProject(isProjectImport: false,
+                                              enterProjectImmediately: true)
+                
+                await MainActor.run {
+                    store?.showsSampleProjectModal = false
+                }
+            }
+            
+        } label: {
+            VStack {
+                Image(systemName: "document.badge.plus.fill")
+                    .projectThumbnailRatio(hasThumbnail: false,
+                                           // Assume gray?
+                                           previewWindowBackgroundColor: .gray)
+                StitchTextView(string: "Blank project")
+            }
+        }
+        .buttonStyle(.borderless)
     }
 }
 
@@ -82,31 +104,33 @@ struct SampleProjectView: View {
     
     @Bindable var store: StitchStore
     
-    let data: SampleProjectData
+    let data: SampleProjectData?
     
     var body: some View {
-        Button {
-            Task(priority: .high) { [weak store] in
-                if let store = store {
-                    await importStitchSampleProject(sampleProjectURL: data.url,
-                                                    store: store)
-                    
-                    await MainActor.run { [weak store] in
-                        store?.showsSampleProjectModal = false
+        if let data = data {
+            Button {
+                Task(priority: .high) { [weak store] in
+                    if let store = store {
+                        await importStitchSampleProject(sampleProjectURL: data.url,
+                                                        store: store)
+                        
+                        await MainActor.run { [weak store] in
+                            store?.showsSampleProjectModal = false
+                        }
                     }
                 }
+                
+            } label: {
+                VStack {
+                    Image(data.imageName)
+                        .projectThumbnailRatio(hasThumbnail: true,
+                                               // Assume gray?
+                                               previewWindowBackgroundColor: .gray)
+                    StitchTextView(string: data.projectName)
+                }
             }
-            
-        } label: {
-            VStack {
-                Image(data.imageName)
-                    .projectThumbnailRatio(hasThumbnail: true,
-                                           // Assume gray?
-                                           previewWindowBackgroundColor: .gray)
-                StitchTextView(string: data.projectName)
-            }
+            .buttonStyle(.borderless)
         }
-        .buttonStyle(.borderless)
     }
 }
 
