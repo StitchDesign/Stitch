b6272820af6a2a994d0929a3b785197978ea0265
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-05-16 15:12:50 -0700

    new test

--- StitchTests/stitchFileTests.swift
+++ StitchTests/stitchFileTests.swift
@@ -9,5 +9,18 @@ import XCTest
 @testable import Stitch
 
 final class StitchFileTests: XCTestCase {
+    @MainActor
+    var mockStore = StitchStore()
     
+    func testSampleProjectOpen() async throws {
+        guard let projects = try? SampleProjectsList.getProjects() else {
+            XCTFail("Sample projects couldn't be retrieved")
+            return
+        }
+        
+        for project in projects {
+            try await importStitchSampleProject(sampleProjectURL: project.url,
+                                                store: mockStore)
+        }
+    }
  }
