1a1918ebc7cbf17a58815fe549a8e4124fb6acdb
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-05-23 21:49:05 -0700

    Updated AI validator scripts for reasoning models (#1279)

--- Stitch/Graph/StitchAI/GraphPrompting/DataModel/StitchAITrainingData.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/DataModel/StitchAITrainingData.swift
@@ -7,38 +7,72 @@
 
 import SwiftUI
 
-struct StitchAITrainingData: Decodable {
+protocol StitchAITrainingDataValidatable: Decodable {
+    static func getTrainingData(from datasetExamples: [Self]) -> [StitchAIActionsTrainingData]
+}
+
+struct StitchAITrainingData {
     let messages: [OpenAIMessage]
+}
+
+extension StitchAITrainingData: StitchAITrainingDataValidatable {
+    static func getTrainingData(from datasetExamples: [Self]) -> [StitchAIActionsTrainingData] {
+        let jsonDecoder = JSONDecoder()
+        
+        let assistantMessages = datasetExamples.compactMap {
+            $0.messages.first { $0.role == .assistant }?.content
+        }
+        
+        let actionsDataList: [StitchAIActionsTrainingData] = assistantMessages
+            .enumerated()
+            .compactMap { (index, message) in
+            guard let messageData = message.data(using: .utf8) else {
+                print("StitchAITraining failure: no data created")
+                return nil
+            }
+            
+            do {
+                let actionsData = try jsonDecoder.decode(StitchAIActionsTrainingData.self, from: messageData)
+                return actionsData
+            } catch {
+                print("StitchAITrainingData validation error with example \(index+1): Step decoding failed with error: \(error)")
+                return nil
+            }
+        }
+        
+        return actionsDataList
+    }
+}
+
+// used by o4-mini
+struct StitchAIReasoningTrainingData: Decodable {
+    let messages: [OpenAIMessage]
+    
+    // used by reasoning models
+    let completion: StitchAIActionsTrainingData
+}
+
+extension StitchAIReasoningTrainingData: StitchAITrainingDataValidatable {
+    static func getTrainingData(from datasetExamples: [Self]) -> [StitchAIActionsTrainingData] {
+        datasetExamples.map(\.completion)
+    }
 }
 
 struct StitchAIActionsTrainingData: Decodable {
     let actions: [Step]
 }
 
-extension StitchAITrainingData {
-    static func validateTrainingData(from filename: String) {
-        let jsonDecoder = JSONDecoder()
-        let trainingData = StitchAITrainingData
-            .decodeTrainingData(from: "stitch-training")!
-        let assistantMessages = trainingData.compactMap {
-            $0.messages.first { $0.role == .assistant }?.content
-        }
-        
-        for (index, message) in assistantMessages.enumerated() {
-            guard let messageData = message.data(using: .utf8) else {
-                log("StitchAITraining failure: no data created")
-                continue
-            }
+enum StitchAITrainingError: Error {
+    case fileNotFound
+}
 
-            let actionsData: StitchAIActionsTrainingData
-            
-            do {
-                actionsData = try jsonDecoder.decode(StitchAIActionsTrainingData.self, from: messageData)
-            } catch {
-                log("StitchAITrainingData validation error with example \(index+1): Step decoding failed with error: \(error)")
-                continue
-            }
-            
+extension StitchAITrainingDataValidatable {
+    static func validateTrainingData(from filename: String) throws {
+        let trainingData = try Self.decodeTrainingData(from: filename)
+        
+        let actionsDataList: [StitchAIActionsTrainingData] = Self.getTrainingData(from: trainingData)
+        
+        for (index, actionsData) in actionsDataList.enumerated() {
             var validationErrors: [String] = []
             for step in actionsData.actions {
                 do {
@@ -49,71 +83,59 @@ extension StitchAITrainingData {
             }
             
             guard validationErrors.isEmpty else {
-                log("StitchAITrainingData validation error with example \(index+1): Step action validation failed with the following errors:\n")
+                print("StitchAITrainingData validation error with example \(index+1): Step action validation failed with the following errors:\n")
                 for errorMessage in validationErrors {
-                    log(errorMessage)
+                    print(errorMessage)
                 }
                 continue
             }
             
-            log("StitchAITrainingData validation successful at \(index + 1)")
+            print("StitchAITrainingData validation successful at \(index + 1)")
         }
     }
     
-    static func decodeTrainingData(from filename: String) -> [Self]? {
-        guard let jsonArray = Self.convertJSONLToJSON(from: filename) else {
-            return nil
-        }
+    static func decodeTrainingData(from filename: String) throws -> [Self] {
+        let jsonArray = try Self.convertJSONLToJSON(from: filename)
         
         let decoder = JSONDecoder()
 
-        do {
-            let data = try JSONSerialization.data(withJSONObject: jsonArray, options: [.prettyPrinted])
-
-            let messages = try decoder.decode([Self].self,
-                                              from: data)
-            
-            return messages
-        } catch {
-            print("Failed to convert array to JSON: \(error.localizedDescription)")
-            return nil
-        }
+        let data = try JSONSerialization.data(withJSONObject: jsonArray, options: [.prettyPrinted])
+        
+        let messages = try decoder.decode([Self].self,
+                                          from: data)
+        
+        return messages
     }
     
-    static func convertJSONLToJSON(from filename: String) -> [Any]? {
+    static func convertJSONLToJSON(from filename: String) throws -> [Any] {
         guard let fileURL = Bundle.main.url(forResource: filename, withExtension: "jsonl") else {
-            return nil
+            throw StitchAITrainingError.fileNotFound
         }
         
-        do {
-            // Read the entire content of the file as a single String
-            let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
+        // Read the entire content of the file as a single String
+        let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
+        
+        // Split the file contents by newlines
+        let lines = fileContents.components(separatedBy: .newlines)
+        
+        // Parse each line into a JSON object
+        var jsonObjects: [Any] = []
+        
+        for line in lines {
+            let trimmedLine = line.trimmingCharacters(in: .whitespacesAndNewlines)
             
-            // Split the file contents by newlines
-            let lines = fileContents.components(separatedBy: .newlines)
+            // Skip empty lines
+            guard !trimmedLine.isEmpty else { continue }
             
-            // Parse each line into a JSON object
-            var jsonObjects: [Any] = []
-            
-            for line in lines {
-                let trimmedLine = line.trimmingCharacters(in: .whitespacesAndNewlines)
-                
-                // Skip empty lines
-                guard !trimmedLine.isEmpty else { continue }
-                
-                // Convert the line to Data
-                if let data = trimmedLine.data(using: .utf8) {
-                    // Deserialize the JSON object
-                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-                    jsonObjects.append(jsonObject)
-                }
+            // Convert the line to Data
+            if let data = trimmedLine.data(using: .utf8) {
+                // Deserialize the JSON object
+                let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                jsonObjects.append(jsonObject)
             }
-            
-            return jsonObjects
-        } catch {
-            print("Failed to convert JSONL file: \(error.localizedDescription)")
-            return nil
         }
+        
+        return jsonObjects
     }
 }
 
