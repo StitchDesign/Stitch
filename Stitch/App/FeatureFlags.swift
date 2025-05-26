2c8b44e5263897b8af0bb8270b2730d9e31f474f
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-05-26 10:59:44 -0700

    feature flag

--- Stitch/App/FeatureFlags.swift
+++ Stitch/App/FeatureFlags.swift
@@ -13,9 +13,16 @@ struct FeatureFlags {
     static let USE_COMMENT_BOX_FLAG: Bool = false
     static let USE_COMPONENTS = false
     static let USE_AI_MODE = true
-    #if STITCH_AI_REASONING
+    
+#if STITCH_AI_REASONING
     static let STITCH_AI_REASONING = true
     #else
     static let STITCH_AI_REASONING = false
+    #endif
+    
+    #if DEV_DEBUG
+    static let ENABLE_JS_NODE = true
+    #else
+    static let ENABLE_JS_NODE = false
     #endif
 }
