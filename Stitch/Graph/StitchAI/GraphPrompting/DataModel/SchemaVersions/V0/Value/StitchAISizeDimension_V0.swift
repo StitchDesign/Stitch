f47b68cd516edc04e1e6d3c09b180db89ed4d93d
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-06-01 22:49:12 -0700

    StitchAISizeDimension

--- Stitch/Graph/StitchAI/GraphPrompting/DataModel/SchemaVersions/V0/Value/StitchAISizeDimension_V0.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/DataModel/SchemaVersions/V0/Value/StitchAISizeDimension_V0.swift
@@ -11,11 +11,12 @@ import StitchSchemaKit
 enum StitchAISizeDimension_V0: StitchSchemaVersionable {
     // MARK: - ensure versions are correct
     static let version = StitchAISchemaVersion._V0
+    typealias LayerDimension = LayerDimension_V31.LayerDimension
     typealias PreviousInstance = Self.StitchAISizeDimension
     // MARK: - end
     
     struct StitchAISizeDimension: StitchAIStringConvertable {
-        var value: LayerDimension_V31.LayerDimension
+        var value: LayerDimension
     }
 }
 
@@ -26,9 +27,9 @@ extension StitchAISizeDimension_V0.StitchAISizeDimension: StitchVersionedCodable
     }
 }
 
-extension StitchAISizeDimension_V0.StitchAISizeDimension {
+extension StitchAISizeDimension_V0.LayerDimension: StitchAIValueStringConvertable {
     var encodableString: String {
-        self.value.description
+        self.description
     }
     
     public init?(_ description: String) {
@@ -37,5 +38,43 @@ extension StitchAISizeDimension_V0.StitchAISizeDimension {
         }
         
         self = result
+    }
+    
+    static func fromUserEdit(edit: String) -> Self? {
+        if edit == LayerDimension.AUTO_SIZE_STRING {
+            return .auto
+        } else if edit == LayerDimension.FILL_SIZE_STRING {
+            return .fill
+        } else if edit == LayerDimension.HUG_SIZE_STRING {
+            return .hug
+        } else if let n = Self.parsePercentage(edit) {
+            return .parentPercent(n)
+        } else if let n = Self.toNumber(edit) {
+            return .number(CGFloat(n))
+        } else {
+            return nil
+        }
+    }
+    
+    private static func parsePercentage(_ edit: String) -> Double? {
+        if let last = edit.last, last == "%" {
+            return toNumber(String(edit.dropLast()))
+        }
+        return nil
+    }
+    
+    private static func toNumber(_ userEdit: String) -> Double? {
+        let result = Double(userEdit)
+        if !result.isDefined {
+            // https://github.com/davedelong/DDMathParser/wiki/Usage
+            if let evalResult = try? userEdit.evaluate() {
+                //            log("toNumber: evalResult: \(evalResult)")
+                return evalResult
+            } else {
+                //            log("toNumber: could not evaluate userEdit \(userEdit)")
+                return nil
+            }
+        }
+        return result
     }
 }
