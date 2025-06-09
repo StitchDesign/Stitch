15bd20e4441db71d0a08dff6cee4f040e058ee58
Chris Clampitt <christian.clampitt@nyu.edu>
2025-06-09 10:44:04 -0700

    Introduce StitchAppClip target

---
+++ StitchAppClip/StitchAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  StitchAppClipApp.swift
+//  StitchAppClip
+//
+//  Created by Christian J Clampitt on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct StitchAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
