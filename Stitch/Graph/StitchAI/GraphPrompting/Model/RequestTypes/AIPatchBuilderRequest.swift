7d772eb7226f4cdc8dc0bca112d4351b420adb03
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-07-09 21:14:37 -0700

    fixed type changing

--- Stitch/Graph/StitchAI/GraphPrompting/Model/RequestTypes/AIPatchBuilderRequest.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/Model/RequestTypes/AIPatchBuilderRequest.swift
@@ -211,7 +211,16 @@ extension CurrentAIPatchBuilderResponseFormat.GraphData {
             
             // Set custom value type here
             if let customValueType = self.patch_data.native_patch_value_type_settings.first(where: { $0.node_id.value == oldId })?.value_type {
-                patchNode.userVisibleType = try customValueType.value.migrate()
+                guard let oldType = newNode.userVisibleType else {
+                    fatalErrorIfDebug()
+                    break
+                }
+                
+                let newType = try customValueType.value.migrate()
+                let _ = document.graph.changeType(for: newNode,
+                                                  oldType: oldType,
+                                                  newType: newType,
+                                                  activeIndex: document.activeIndex)
             }
             
             // MARK: BEFORE creating edges/inputs, determine if new patch nodes need extra inputs
