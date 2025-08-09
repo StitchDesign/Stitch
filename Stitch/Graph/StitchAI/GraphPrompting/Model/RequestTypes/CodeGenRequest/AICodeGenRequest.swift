9235f26d651d5d0010621218c2c60a24109d50c2
pianostringquartet <christian.clampitt@nyu.edu>
2025-08-08 19:05:40 -0700

    remove unused .imagePrompt code

--- Stitch/Graph/StitchAI/GraphPrompting/Model/RequestTypes/CodeGenRequest/AICodeGenRequest.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/Model/RequestTypes/CodeGenRequest/AICodeGenRequest.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// fka `AICodeGenFromGraphRequest`
 struct AICodeGenWithImageRequest: StitchAICodeCreator {
     static let type = StitchAIRequestBuilder_V0.StitchAIRequestType.userPrompt
     
