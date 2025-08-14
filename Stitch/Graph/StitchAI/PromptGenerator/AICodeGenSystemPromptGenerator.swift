34c7f76491de834789651aadb5f26a5be66e9d88
pianostringquartet <christian.clampitt@nyu.edu>
2025-08-13 22:53:19 -0600

    Explicitly blacklist overlay and background view modifiers

--- Stitch/Graph/StitchAI/PromptGenerator/AICodeGenSystemPromptGenerator.swift
+++ Stitch/Graph/StitchAI/PromptGenerator/AICodeGenSystemPromptGenerator.swift
@@ -392,6 +392,8 @@ You are ONLY permitted to use these view modifiers. Do not attempt to use view m
 Stitch doesn't support usage of the following view modifiers:
 * `gesture`: only `simultaneousGesture` is allowed.
 * `animation`: instead use native animation patch nodes like "classicAnimation || Patch" or "springAnimation || Patch"
+* `overlay`: instead, use a ZStack
+* `background`: instead, use a ZStack 
 
 ### Other Disallowed Behavior
 In most scenarios, you should not need to replicate functionality that would involve usage of class objects or usage of libraries other than SwiftUI. Native patch nodes largely handle these scenarios for you. Each listed scenario must use native patch nodes.
