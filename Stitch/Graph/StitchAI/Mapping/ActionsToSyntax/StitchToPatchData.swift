43b67f0d61b4e5d649800f94ae0098481257c153
pianostringquartet <christian.clampitt@nyu.edu>
2025-08-19 17:36:33 -0600

    replace spaces with _, to create code-safe var names

--- Stitch/Graph/StitchAI/Mapping/ActionsToSyntax/StitchToPatchData.swift
+++ Stitch/Graph/StitchAI/Mapping/ActionsToSyntax/StitchToPatchData.swift
@@ -71,7 +71,12 @@ extension GraphEntity {
 
 extension String {
     func createUniqueVarName(nodeId: UUID) -> String {
-        "\(self)_\(nodeId.uuidString)"
+//        let camelCased = self.toCamelCase() // turns "Z Index" into "zIndex"
+//        return "\(camelCased)_\(nodeId.uuidString)"
+//            .replacingOccurrences(of: "-", with: "_")
+        
+        return "\(self)_\(nodeId.uuidString)"
+            .replacingOccurrences(of: " ", with: "_")
             .replacingOccurrences(of: "-", with: "_")
     }
 }
