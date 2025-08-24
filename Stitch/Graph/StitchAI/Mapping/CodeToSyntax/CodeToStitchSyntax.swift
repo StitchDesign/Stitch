c3f4c889708e128e1165281259da3f939e6733e9
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-08-24 10:59:01 -0700

    fixed the contains op

--- Stitch/Graph/StitchAI/Mapping/CodeToSyntax/CodeToStitchSyntax.swift
+++ Stitch/Graph/StitchAI/Mapping/CodeToSyntax/CodeToStitchSyntax.swift
@@ -187,7 +187,7 @@ final class SwiftUIViewVisitor: SyntaxVisitor {
     /// Ensures we only parse view structs.
     override func visit(_ node: StructDeclSyntax) -> SyntaxVisitorContinueKind {
         guard let inheritanceClause = node.inheritanceClause,
-              inheritanceClause.inheritedTypes.contains(where: { $0.type.trimmedDescription.contains("some View") }) else {
+              inheritanceClause.inheritedTypes.contains(where: { $0.type.trimmedDescription.contains("View") }) else {
             return .skipChildren
         }
         return .visitChildren
