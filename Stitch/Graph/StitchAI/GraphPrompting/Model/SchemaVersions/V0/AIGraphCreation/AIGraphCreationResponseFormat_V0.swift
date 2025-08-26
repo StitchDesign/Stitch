aa51bdf39ba8252e02f7225207a67fb7440ab80e
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-08-25 20:46:23 -0700

    excluded gesture patches from system prompt

--- Stitch/Graph/StitchAI/GraphPrompting/Model/SchemaVersions/V0/AIGraphCreation/AIGraphCreationResponseFormat_V0.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/Model/SchemaVersions/V0/AIGraphCreation/AIGraphCreationResponseFormat_V0.swift
@@ -58,7 +58,7 @@ enum AIGraphCreationResponseFormat_V0 {
                                     description: "Array of node UUIDs",
                                     items: OpenAIGeneric(types: [OpenAISchema(type: .string)]))
         
-        let NodeName = OpenAISchemaEnum(values: Step_V0.NodeKind.getAiNodeDescriptions().map(\.nodeKind))
+        let NodeName = OpenAISchemaEnum(values: AIGraphData_V0.NodeKind.getAiNodeDescriptions().map(\.nodeKind))
         
         let ValueType = OpenAISchemaEnum(values:
                                             StitchAINodeType.allCases
