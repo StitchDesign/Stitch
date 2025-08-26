fb7b5170699be9373ad9ff1ccb0e1d7a6b35b8db
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-08-25 18:32:34 -0700

    more system prompt removals

--- Stitch/Graph/StitchAI/PromptGenerator/AICodeEditSystemPromptGenerator.swift
+++ Stitch/Graph/StitchAI/PromptGenerator/AICodeEditSystemPromptGenerator.swift
@@ -32,10 +32,8 @@ ScrollView([.vertical]) {
     VStack {
         Rectangle()
             .fill(rectColors)
-            .layerId("76A53AE1-7B9F-48EA-8BB1-23CF7B74FFFF")
     }
 }
-.layerId("1D822183-260F-4997-9AB5-C896B00C013C")
 ```
 
 Do not modify the existing scroll view as such:
@@ -45,13 +43,10 @@ ScrollView([.vertical]) {
     VStack {
         Rectangle()
             .fill(rectColors)
-            .layerId("76A53AE1-7B9F-48EA-8BB1-23CF7B74FFFF")
         Oval()
             .fill(PortValueDescription(value: "#FFC0CBFF", value_type: "color"))
-            .layerId("E739BE1F-3A2B-4C1D-8F6E-1234567890AB")
     }
 }
-.layerId("1D822183-260F-4997-9AB5-C896B00C013C")
 ```
 
 Instead, use a `ZStack`:
@@ -62,215 +57,17 @@ ZStack {
         VStack {
             Rectangle()
                 .fill(rectColors)
-                .layerId("76A53AE1-7B9F-48EA-8BB1-23CF7B74FFFF")
         }
     }
-    .layerId("1D822183-260F-4997-9AB5-C896B00C013C")
 
     Oval()
         .fill(PortValueDescription(value: "#FFC0CBFF", value_type: "color"))
-        .layerId("E739BE1F-3A2B-4C1D-8F6E-1234567890AB")
-}
-.layerId("1AA4B943-9442-4D36-A380-525F65D8449E")
-```
-
-
-Similarly, if we are asked to "now change the colors each time a rectangle is tapped" and the `updateLayerInputs` function in the original code had a declared Loop patch, we should keep that Loop around.
-
-Original code's `updateLayerInputs`:
-
-```swift
-struct ContentView: some View {
-    @State var Position_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-    @State var Color_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-    @State var ZIndex_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-
-    var body: some View {
-        ScrollView(showsIndicators: nil) {
-    Rectangle()
-    .layerId("81DD4E04-FF32-4CA9-B406-908806A38211")
-    .fill(Color_81DD4E04_FF32_4CA9_B406_908806A38211)
-  .position(Position_81DD4E04_FF32_4CA9_B406_908806A38211)
-  .frame([PortValueDescription(value_type:"size",value:{height:"50.0",width:"50.0"})])
-}
-    .layerId("DC97EBBC-E40B-4E97-A095-E85ECBF3174F")
     
-    }
-
-    func updateLayerInputs() {
-        let loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD = NATIVE_STITCH_PATCH_FUNCTIONS["loop || Patch"]([
-            [PortValueDescription(value:100,value_type:"number")]
-        ])
-  let random_1827964F_6EE2_406B_9946_63D5DE7F48BA = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-        loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD[0],
-  [PortValueDescription(value_type:"number",value:0)],
-  [PortValueDescription(value_type:"number",value:1)]
-    ])
-  let random_310E050E_B293_4BD2_BADA_84C851A6200F = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-        loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD[0],
-  [PortValueDescription(value_type:"number",value:0)],
-  [PortValueDescription(value:1,value_type:"number")]
-    ])
-  let random_83E8C168_C301_466D_9F80_4FC6B7DD348C = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-        loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD[0],
-  [PortValueDescription(value_type:"number",value:0)],
-  [PortValueDescription(value_type:"number",value:1)]
-    ])
-  let dragInteraction_98600A64_16DB_4305_8DFB_5BF5A309D2E6 = NATIVE_STITCH_PATCH_FUNCTIONS["dragInteraction || Patch"]([
-        [PortValueDescription(value:"81DD4E04-FF32-4CA9-B406-908806A38211",value_type:"layer")],
-  [PortValueDescription(value_type:"bool",value:true)],
-  [PortValueDescription(value_type:"bool",value:false)],
-  [PortValueDescription(value_type:"position",value:{x:0,y:0})],
-  [PortValueDescription(value:0,value_type:"pulse")],
-  [PortValueDescription(value:false,value_type:"bool")],
-  [PortValueDescription(value_type:"position",value:{x:0,y:0})],
-  [PortValueDescription(value:{y:0,x:0},value_type:"position")]
-    ])
-  let rgba_BA714894_D38F_4CD6_829C_75D1BC49C5B9 = NATIVE_STITCH_PATCH_FUNCTIONS["rgbColor || Patch"]([
-        random_1827964F_6EE2_406B_9946_63D5DE7F48BA[0],
-  random_83E8C168_C301_466D_9F80_4FC6B7DD348C[0],
-  random_310E050E_B293_4BD2_BADA_84C851A620[[0]]F[0],
-  [PortValueDescription(value:1,value_type:"number")]
-    ])
-    Position_81DD4E04_FF32_4CA9_B406_908806A38211 = dragInteraction_98600A64_16DB_4305_8DFB_5BF5A309D2E6[0]
-    Color_81DD4E04_FF32_4CA9_B406_908806A38211 = rgba_BA714894_D38F_4CD6_829C_75D1BC49C5B9[0]
-    ZIndex_81DD4E04_FF32_4CA9_B406_908806A38211 = loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD[0]
-    }
-}
-``` 
-
-BAD EDIT: we lose the Loop in the original code's `updateLayerInputs`:
-
-```
-struct ContentView: View {
-    @State var Position_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-    @State var Color_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-
-    var body: some View {
-        ScrollView(showsIndicators: nil) {
-            Rectangle()
-                .fill(Color_81DD4E04_FF32_4CA9_B406_908806A38211)
-                .position(Position_81DD4E04_FF32_4CA9_B406_908806A38211)
-                .frame([PortValueDescription(value: ["height":"50.0","width":"50.0"], value_type: "size")])
-                .layerId("81DD4E04-FF32-4CA9-B406-908806A38211")
-        }
-        .layerId("DC97EBBC-E40B-4E97-A095-E85ECBF3174F")
-    }
-
-    func updateLayerInputs() {
-        let dragInteraction_98600A64_16DB_4305_8DFB_5BF5A309D2E6 = NATIVE_STITCH_PATCH_FUNCTIONS["dragInteraction || Patch"]([
-            [PortValueDescription(value: "81DD4E04-FF32-4CA9-B406-908806A38211", value_type: "layer")],
-            [PortValueDescription(value: true, value_type: "bool")],
-            [PortValueDescription(value: false, value_type: "bool")],
-            [PortValueDescription(value: ["x":0,"y":0], value_type: "position")],
-            [PortValueDescription(value: 0, value_type: "pulse")],
-            [PortValueDescription(value: false, value_type: "bool")],
-            [PortValueDescription(value: ["x":0,"y":0], value_type: "position")],
-            [PortValueDescription(value: ["x":0,"y":0], value_type: "position")]
-        ])
-        let pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A = NATIVE_STITCH_PATCH_FUNCTIONS["pressInteraction || Patch"]([
-            [PortValueDescription(value: "81DD4E04-FF32-4CA9-B406-908806A38211", value_type: "layer")],
-            [PortValueDescription(value: true, value_type: "bool")],
-            [PortValueDescription(value: 0, value_type: "number")]
-        ])
-        let randomR_3BC9D2F5_6E41_4E20_9B74_77D5E1F91A2B = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-            pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A[1],
-            [PortValueDescription(value: 0, value_type: "number")],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-        let randomG_8F4A1C2E_C3B9_4EE4_8F96_23D1BF7A3C4D = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-            pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A[1],
-            [PortValueDescription(value: 0, value_type: "number")],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-        let randomB_A1D2E3F4_5B6C_7D8E_9F01_23456789ABCD = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-            pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A[1],
-            [PortValueDescription(value: 0, value_type: "number")],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-        let rgbColor_BA714894_D38F_4CD6_829C_75D1BC49C5B9 = NATIVE_STITCH_PATCH_FUNCTIONS["rgbColor || Patch"]([
-            randomR_3BC9D2F5_6E41_4E20_9B74_77D5E1F91A2B[0],
-            randomG_8F4A1C2E_C3B9_4EE4_8F96_23D1BF7A3C4D[0],
-            randomB_A1D2E3F4_5B6C_7D8E_9F01_23456789ABCD[0],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-
-        Position_81DD4E04_FF32_4CA9_B406_908806A38211 = dragInteraction_98600A64_16DB_4305_8DFB_5BF5A309D2E6[0]
-        Color_81DD4E04_FF32_4CA9_B406_908806A38211 = rgbColor_BA714894_D38F_4CD6_829C_75D1BC49C5B9[0]
-    }
-}
-```
-
-GOOD EDIT: we KEEP the Loop in the original code's `updateLayerInputs`:
-
-```
-struct ContentView: View {
-    @State var Position_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-    @State var Color_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-    @State var ZIndex_81DD4E04_FF32_4CA9_B406_908806A38211: [PortValueDescription] = []
-
-    var body: some View {
-        ScrollView(showsIndicators: nil) {
-            Rectangle()
-                .fill(Color_81DD4E04_FF32_4CA9_B406_908806A38211)
-                .position(Position_81DD4E04_FF32_4CA9_B406_908806A38211)
-                .frame([PortValueDescription(value: ["height":"50.0","width":"50.0"], value_type: "size")])
-                .layerId("81DD4E04-FF32-4CA9-B406-908806A38211")
-        }
-        .layerId("DC97EBBC-E40B-4E97-A095-E85ECBF3174F")
-    }
-
-    func updateLayerInputs() {
-        let loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD = NATIVE_STITCH_PATCH_FUNCTIONS["loop || Patch"]([
-            [PortValueDescription(value:100,value_type:"number")]
-        ])
-
-        let dragInteraction_98600A64_16DB_4305_8DFB_5BF5A309D2E6 = NATIVE_STITCH_PATCH_FUNCTIONS["dragInteraction || Patch"]([
-            [PortValueDescription(value: "81DD4E04-FF32-4CA9-B406-908806A38211", value_type: "layer")],
-            [PortValueDescription(value: true, value_type: "bool")],
-            [PortValueDescription(value: false, value_type: "bool")],
-            [PortValueDescription(value: ["x":0,"y":0], value_type: "position")],
-            [PortValueDescription(value: 0, value_type: "pulse")],
-            [PortValueDescription(value: false, value_type: "bool")],
-            [PortValueDescription(value: ["x":0,"y":0], value_type: "position")],
-            [PortValueDescription(value: ["x":0,"y":0], value_type: "position")]
-        ])
-        let pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A = NATIVE_STITCH_PATCH_FUNCTIONS["pressInteraction || Patch"]([
-            [PortValueDescription(value: "81DD4E04-FF32-4CA9-B406-908806A38211", value_type: "layer")],
-            [PortValueDescription(value: true, value_type: "bool")],
-            [PortValueDescription(value: 0, value_type: "number")]
-        ])
-        let randomR_3BC9D2F5_6E41_4E20_9B74_77D5E1F91A2B = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-            pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A[1],
-            [PortValueDescription(value: 0, value_type: "number")],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-        let randomG_8F4A1C2E_C3B9_4EE4_8F96_23D1BF7A3C4D = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-            pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A[1],
-            [PortValueDescription(value: 0, value_type: "number")],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-        let randomB_A1D2E3F4_5B6C_7D8E_9F01_23456789ABCD = NATIVE_STITCH_PATCH_FUNCTIONS["random || Patch"]([
-            pressInteraction_5A1F7E38_23A1_4F5B_A3C1_B8CBBF4D2E7A[1],
-            [PortValueDescription(value: 0, value_type: "number")],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-        let rgbColor_BA714894_D38F_4CD6_829C_75D1BC49C5B9 = NATIVE_STITCH_PATCH_FUNCTIONS["rgbColor || Patch"]([
-            randomR_3BC9D2F5_6E41_4E20_9B74_77D5E1F91A2B[0],
-            randomG_8F4A1C2E_C3B9_4EE4_8F96_23D1BF7A3C4D[0],
-            randomB_A1D2E3F4_5B6C_7D8E_9F01_23456789ABCD[0],
-            [PortValueDescription(value: 1, value_type: "number")]
-        ])
-
-        Position_81DD4E04_FF32_4CA9_B406_908806A38211 = dragInteraction_98600A64_16DB_4305_8DFB_5BF5A309D2E6[0]
-        Color_81DD4E04_FF32_4CA9_B406_908806A38211 = rgbColor_BA714894_D38F_4CD6_829C_75D1BC49C5B9[0]
-        ZIndex_81DD4E04_FF32_4CA9_B406_908806A38211 = loop_7DF8AC6C_2143_4E07_9BB5_F12C80A03FCD[0]
-    }
 }
 ```
 
 # Code Generation Rules
-Adhere to the following guideliens:
+Adhere to the following guidelines:
 
 \(try StitchAIManager.aiCodeGenSystemPromptGenerator(requestType: requestType, previewWindowSize: previewWindowSize, previewWindowBackgroundColor: previewWindowBackgroundColor))
 
