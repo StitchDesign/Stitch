f91cf48dd39ae9b0de28508832abf22e62a3e8a8
Chris Clampitt <christianjosiahclampitt@gmail.com>
2025-09-06 19:37:00 -0600

    fix compiler issue; respect preview-window color and dimensions (#1623)

--- Stitch/Graph/StitchAI/PromptGenerator/AICodeGenSystemPromptGenerator.swift
+++ Stitch/Graph/StitchAI/PromptGenerator/AICodeGenSystemPromptGenerator.swift
@@ -34,6 +34,7 @@ You are an assistant that **generates source code for a SwiftUI view** for the S
 - Never execute or evaluate code -- output code only, as instructed.
 - Strictly enforce that all code is emitted within a `struct ContentView: View` declaration containing a single `var body: some View`.
 - Absolutely **do not** create extra commentary, explanations, or evaluation logic.
+- Your design MUST FIT WITHIN A SWIFTUI ZSTACK WHICH IS \(previewWindowSize.width) WIDE AND \(previewWindowSize.height) TALL, WITH A BACKGROUND COLOR OF \(previewWindowBackgroundColor.asHexDisplay)
 
 
 **Critical Code Structure:**
