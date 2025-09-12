36e470221f9db1e2122f1f66cb287dc411b6f244
Chris Clampitt <christianjosiahclampitt@gmail.com>
2025-09-12 13:58:15 -0600

    Wipe pulse outputs on graph reset (#1635)

--- Stitch/Graph/Node/Port/ViewModel/NodeRowObserver/NodeRowObserverSchemaObserverIdentifiable.swift
+++ Stitch/Graph/Node/Port/ViewModel/NodeRowObserver/NodeRowObserverSchemaObserverIdentifiable.swift
@@ -86,10 +86,22 @@ extension InputNodeRowObserver: SchemaObserverIdentifiable {
 }
 
 extension OutputNodeRowObserver {
+
     @MainActor
     func onPrototypeRestart(document: StitchDocumentViewModel) {
         // Set outputs to be empty
         // MARK: no longer seems necessary, removing for fixing flashing media on restart
 //        self.allLoopedValues = []
+  
+//        let containsMedia = self.allLoopedValues.first?.asyncMedia.isDefined ?? false
+//        if !containsMedia {
+//            self.allLoopedValues = []
+//        }
+        
+        // TODO: better to only reset based on pulses, since some nodes may still look at old-style previous outputs?
+        let containsPulse = self.allLoopedValues.first?.getPulse.isDefined ?? false
+        if containsPulse {
+            self.allLoopedValues = []
+        }
     }
 }
