0c073765317c875cd39718b17f7a22fcbc02f507
pianostringquartet <christian.clampitt@nyu.edu>
2025-09-16 15:21:08 -0600

    use USE_SWIFTUI_IMPLEMENTATION=true again

--- Stitch/App/FeatureFlags.swift
+++ Stitch/App/FeatureFlags.swift
@@ -17,8 +17,8 @@ struct FeatureFlags {
     // For changes that move Stitch's implementation details closer to SwiftUI,
     // but which may not be good to expose to most beta testers quite yet.
     // TODO: set false for
-//    static let USE_SWIFTUI_IMPLEMENTATION: Bool = true
-    static let USE_SWIFTUI_IMPLEMENTATION: Bool = false
+    static let USE_SWIFTUI_IMPLEMENTATION: Bool = true
+//    static let USE_SWIFTUI_IMPLEMENTATION: Bool = false
     
     // TODO: SET FALSE BEFORE NEXT RELEASE / just use Stitch AI Reasoning ?
     static let SHOW_AI_TABLE_ROWS_VIEWER = true
