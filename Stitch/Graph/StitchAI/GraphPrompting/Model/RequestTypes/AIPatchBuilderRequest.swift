b3d471dede2e63d601c6e0e4d7086dc2bf2306b9
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-09-15 20:07:14 -0700

    connection data for layers

--- Stitch/Graph/StitchAI/GraphPrompting/Model/RequestTypes/AIPatchBuilderRequest.swift
+++ Stitch/Graph/StitchAI/GraphPrompting/Model/RequestTypes/AIPatchBuilderRequest.swift
@@ -148,25 +148,26 @@ extension Array where Element == AIGraphData_V0.LayerData {
                 .createDefaultLayerNodeEntity(nodeId: UUID(layerData.node_id) ?? UUID(),
                                               layerGroupId: layerGroupId)
             
-            layerData.custom_layer_input_values.forEach { portDerivation in
-                let coordinate = portDerivation.coordinate
-                
-                portDerivation.inputData.forEach { inputData in
-                    // TODO: likely need to look at using the input coordinate here to save normal layer specific data
-                    
-                    // Parse actions at this input, which may include patch data in the event of view events
-                    nodesDict.updateWithEventData(inputData,
-                                                  varName: nil,
-                                                  stateVarConnections: &stateVarConnections)
-                }
-            }
-            
             let nodeEntity = NodeEntity(id: layerNodeEntity.id,
                                         nodeTypeEntity: .layer(layerNodeEntity),
                                         title: layerData.suggested_title ?? "")
             
             nodesDict.updateValue(nodeEntity,
                                   forKey: layerNodeEntity.id)
+
+            layerData.custom_layer_input_values.forEach { portDerivation in
+                let coordinate = portDerivation.coordinate
+                
+                portDerivation.inputData.forEach { inputData in
+                    // Parse actions at this input, which may include patch data in the event of view events
+                    nodesDict.updateWithEventData(inputData,
+                                                  layerInputCoordinate: .init(portType: .keyPath(coordinate),
+                                                                              nodeId: layerNodeEntity.id),
+                                                  varName: nil,
+                                                  stateVarConnections: &stateVarConnections)
+                }
+            }
+            
             
             if let children = layerData.children {
                 children
